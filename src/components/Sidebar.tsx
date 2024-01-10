283c8071f9a10f7834910edbb5f6b239ae2a5bc6
Lester Ong <lesterong776@gmail.com>
2024-01-10 15:14:20 +0800

    Setup Sidepanel for each tab

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,16 +3,26 @@ import Sidepanel from './Sidepanel';
 
 const Sidebar = () => {
   const [activePage, setActivePage] = useState('');
-
-  const handleClick = (page: string) => {
-    console.log(page);
-    setActivePage(page);
-  };
+  const handleClick = (page: string) => setActivePage(page);
 
   return (
     <div className="sidebar">
       <Sidenav activePage={activePage} handleClick={handleClick}></Sidenav>
-      <Sidepanel></Sidepanel>
+      {activePage === 'about-nus-campus' && <Sidepanel.AboutNUSCampus></Sidepanel.AboutNUSCampus>}
+      {activePage === 'street-centerlines' && <Sidepanel.StreetCenterlines></Sidepanel.StreetCenterlines>}
+      {activePage === 'building-footprints' && <Sidepanel.BuildingFootprints></Sidepanel.BuildingFootprints>}
+      {activePage === 'green-spaces' && <Sidepanel.GreenSpaces></Sidepanel.GreenSpaces>}
+      {activePage === 'water-bodies' && <Sidepanel.WaterBodies></Sidepanel.WaterBodies>}
+      {activePage === 'osm-buildings' && <Sidepanel.OSMBuildings></Sidepanel.OSMBuildings>}
+      {activePage === 'rhino-models' && <Sidepanel.RhinoModels></Sidepanel.RhinoModels>}
+      {activePage === 'bim-models' && <Sidepanel.BIMModels></Sidepanel.BIMModels>}
+      {activePage === 'buildings' && <Sidepanel.Buildings></Sidepanel.Buildings>}
+      {activePage === 'energy' && <Sidepanel.Energy></Sidepanel.Energy>}
+      {activePage === 'thermal-comfort' && <Sidepanel.ThermalComfort></Sidepanel.ThermalComfort>}
+      {activePage === 'wind' && <Sidepanel.Wind></Sidepanel.Wind>}
+      {activePage === 'solar' && <Sidepanel.Solar></Sidepanel.Solar>}
+      {activePage === 'help' && <Sidepanel.Help></Sidepanel.Help>}
+      {activePage === 'settings' && <Sidepanel.Settings></Sidepanel.Settings>}
     </div>
   );
 };
--- src/components/Sidepanel.tsx
+++ src/components/Sidepanel.tsx
@@ -1,7 +1,155 @@
 import React from 'react';
 
-const Sidepanel = () => {
-  return <div className="sidepanel">Sidepanel</div>;
+interface SidepanelProps {
+  title: string;
+  children: React.ReactNode;
+}
+
+const Sidepanel = ({ title, children }: SidepanelProps) => {
+  return (
+    <div className="sidepanel">
+      <div>
+        <h2>{title}</h2>
+      </div>
+      <>{children}</>
+    </div>
+  );
 };
 
-export default Sidepanel;
+const AboutNUSCampus = () => {
+  return (
+    <Sidepanel title="About NUS Campus">
+      <div>Hello World!</div>
+    </Sidepanel>
+  );
+};
+
+const StreetCenterlines = () => {
+  return (
+    <Sidepanel title="Layers: Street Centerlines">
+      <div>This feature is under construction.</div>
+    </Sidepanel>
+  );
+};
+
+const BuildingFootprints = () => {
+  return (
+    <Sidepanel title="Layers: Building Footprints">
+      <div>This feature is under construction.</div>
+    </Sidepanel>
+  );
+};
+
+const GreenSpaces = () => {
+  return (
+    <Sidepanel title="Layers: Green Spaces">
+      <div>This feature is under construction.</div>
+    </Sidepanel>
+  );
+};
+
+const WaterBodies = () => {
+  return (
+    <Sidepanel title="Layers: Water Bodies">
+      <div>This feature is under construction.</div>
+    </Sidepanel>
+  );
+};
+
+const OSMBuildings = () => {
+  return (
+    <Sidepanel title="Layers: OSM Buildings">
+      <div>This feature is under construction.</div>
+    </Sidepanel>
+  );
+};
+
+const RhinoModels = () => {
+  return (
+    <Sidepanel title="Layers: Rhino Models">
+      <div>This feature is under construction.</div>
+    </Sidepanel>
+  );
+};
+
+const BIMModels = () => {
+  return (
+    <Sidepanel title="Layers: BIM Models">
+      <div>This feature is under construction.</div>
+    </Sidepanel>
+  );
+};
+
+const Buildings = () => {
+  return (
+    <Sidepanel title="Controls: Buildings">
+      <div>This feature is under construction.</div>
+    </Sidepanel>
+  );
+};
+
+const Energy = () => {
+  return (
+    <Sidepanel title="Controls: Energy">
+      <div>This feature is under construction.</div>
+    </Sidepanel>
+  );
+};
+
+const ThermalComfort = () => {
+  return (
+    <Sidepanel title="Controls: Thermal Comfort">
+      <div>This feature is under construction.</div>
+    </Sidepanel>
+  );
+};
+
+const Wind = () => {
+  return (
+    <Sidepanel title="Controls: Wind">
+      <div>This feature is under construction.</div>
+    </Sidepanel>
+  );
+};
+
+const Solar = () => {
+  return (
+    <Sidepanel title="Controls: Solar">
+      <div>This feature is under construction.</div>
+    </Sidepanel>
+  );
+};
+
+const Help = () => {
+  return (
+    <Sidepanel title="Help">
+      <div>This feature is under construction.</div>
+    </Sidepanel>
+  );
+};
+
+const Settings = () => {
+  return (
+    <Sidepanel title="Settings">
+      <div>This feature is under construction.</div>
+    </Sidepanel>
+  );
+};
+
+export default {
+  AboutNUSCampus,
+  StreetCenterlines,
+  BuildingFootprints,
+  GreenSpaces,
+  WaterBodies,
+  OSMBuildings,
+  RhinoModels,
+  BIMModels,
+  Buildings,
+  Energy,
+  ThermalComfort,
+  Wind,
+  Solar,
+  Help,
+  Settings,
+};
