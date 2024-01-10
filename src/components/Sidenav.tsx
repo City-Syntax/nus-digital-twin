0d87e32f30bf16f71da556c05a452430a1acdd36
Lester Ong <lesterong776@gmail.com>
2024-01-10 14:23:37 +0800

    Add active navigation for sidenav

--- src/components/Sidenav.tsx
+++ src/components/Sidenav.tsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Sidenav = () => {
+  const [page, setPage] = useState('');
+
+  const handleClick = (page: string) => {
+    console.log(page);
+    setPage(page);
+  };
+
   return (
     <nav className="nav">
       <div className="nav-header">
@@ -9,34 +16,49 @@ const Sidenav = () => {
       </div>
       <div className="nav-group">
         <div>About</div>
-        <button type="button">About NUS Campus</button>
+        <NavLink activePage={page} label="About NUS Campus" handleClick={handleClick}></NavLink>
       </div>
       <div className="nav-group">
         <div>GIS Layers</div>
-        <button type="button">Street Centerlines</button>
-        <button type="button">Building Footprints</button>
-        <button type="button">Green Spaces</button>
-        <button type="button">Water Bodies</button>
+        <NavLink activePage={page} label="Street Centerlines" handleClick={handleClick}></NavLink>
+        <NavLink activePage={page} label="Building Footprints" handleClick={handleClick}></NavLink>
+        <NavLink activePage={page} label="Green Spaces" handleClick={handleClick}></NavLink>
+        <NavLink activePage={page} label="Water Bodies" handleClick={handleClick}></NavLink>
       </div>
       <div className="nav-group">
         <div>Building Data Layers</div>
-        <button type="button">OSM Buildings</button>
-        <button type="button">Rhino Models</button>
-        <button type="button">BIM Models</button>
+        <NavLink activePage={page} label="OSM Buildings" handleClick={handleClick}></NavLink>
+        <NavLink activePage={page} label="Rhino Models" handleClick={handleClick}></NavLink>
+        <NavLink activePage={page} label="BIM Models" handleClick={handleClick}></NavLink>
       </div>
       <div className="nav-group">
         <div>Controls</div>
-        <button type="button">Buildings</button>
-        <button type="button">Energy</button>
-        <button type="button">Thermal Comfort</button>
-        <button type="button">Wind</button>
-        <button type="button">Solar</button>
+        <NavLink activePage={page} label="Buildings" handleClick={handleClick}></NavLink>
+        <NavLink activePage={page} label="Energy" handleClick={handleClick}></NavLink>
+        <NavLink activePage={page} label="Thermal Comfort" handleClick={handleClick}></NavLink>
+        <NavLink activePage={page} label="Wind" handleClick={handleClick}></NavLink>
+        <NavLink activePage={page} label="Solar" handleClick={handleClick}></NavLink>
       </div>
       <div className="nav-group">
-        <button type="button">Help</button>
-        <button type="button">Settings</button>
+        <NavLink activePage={page} label="Help" handleClick={handleClick}></NavLink>
+        <NavLink activePage={page} label="Settings" handleClick={handleClick}></NavLink>
       </div>
     </nav>
+  );
+};
+
+interface Props {
+  activePage: string;
+  label: string;
+  handleClick: (page: string) => void;
+}
+
+const NavLink = ({ activePage, label, handleClick }: Props) => {
+  const pageName = label.toLowerCase().split(' ').join('-');
+  return (
+    <button className={activePage === pageName ? 'active' : ''} type="button" onClick={() => handleClick(pageName)}>
+      {label}
+    </button>
   );
 };
 
