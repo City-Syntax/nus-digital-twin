997c70ff9b67dc75c8cbf6d597afd623e2fff003
Lester Ong <lesterong776@gmail.com>
2024-01-22 01:11:09 +0800

    Add close button

--- src/components/LeftPanel.tsx
+++ src/components/LeftPanel.tsx
@@ -3,135 +3,154 @@ import Styles from '../styles/Navigation.module.css';
 
 interface LeftPanelProps {
   title: string;
+  handleClick: () => void;
   children?: React.ReactNode;
 }
 
-const LeftPanel = ({ title, children }: LeftPanelProps) => {
+interface LeftPanelChildProps {
+  handleClick: () => void;
+}
+
+const LeftPanel = ({ title, handleClick, children }: LeftPanelProps) => {
   return (
     <div className={`${Styles['left-panel']} ${Styles['nav-content']}`}>
       <div>
         <h2>{title}</h2>
+        <button type="button" onClick={handleClick}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <line x1="18" y1="6" x2="6" y2="18"></line>
+            <line x1="6" y1="6" x2="18" y2="18"></line>
+          </svg>
+        </button>
       </div>
       <>{children}</>
     </div>
   );
 };
 
-const AboutNUSCampus = () => {
+const AboutNUSCampus = ({ handleClick }: LeftPanelChildProps) => {
   return (
-    <LeftPanel title="About NUS Campus">
+    <LeftPanel title="About NUS Campus" handleClick={handleClick}>
       <div>Hello World!</div>
     </LeftPanel>
   );
 };
 
-const StreetCenterlines = () => {
+const StreetCenterlines = ({ handleClick }: LeftPanelChildProps) => {
   return (
-    <LeftPanel title="Layers: Street Centerlines">
+    <LeftPanel title="Layers: Street Centerlines" handleClick={handleClick}>
       <div>This feature is under construction.</div>
     </LeftPanel>
   );
 };
 
-const BuildingFootprints = () => {
+const BuildingFootprints = ({ handleClick }: LeftPanelChildProps) => {
   return (
-    <LeftPanel title="Layers: Building Footprints">
+    <LeftPanel title="Layers: Building Footprints" handleClick={handleClick}>
       <div>This feature is under construction.</div>
     </LeftPanel>
   );
 };
 
-const GreenSpaces = () => {
+const GreenSpaces = ({ handleClick }: LeftPanelChildProps) => {
   return (
-    <LeftPanel title="Layers: Green Spaces">
+    <LeftPanel title="Layers: Green Spaces" handleClick={handleClick}>
       <div>This feature is under construction.</div>
     </LeftPanel>
   );
 };
 
-const WaterBodies = () => {
+const WaterBodies = ({ handleClick }: LeftPanelChildProps) => {
   return (
-    <LeftPanel title="Layers: Water Bodies">
+    <LeftPanel title="Layers: Water Bodies" handleClick={handleClick}>
       <div>This feature is under construction.</div>
     </LeftPanel>
   );
 };
 
-const OSMBuildings = () => {
+const OSMBuildings = ({ handleClick }: LeftPanelChildProps) => {
   return (
-    <LeftPanel title="Layers: OSM Buildings">
+    <LeftPanel title="Layers: OSM Buildings" handleClick={handleClick}>
       <div>This feature is under construction.</div>
     </LeftPanel>
   );
 };
 
-const RhinoModels = () => {
+const RhinoModels = ({ handleClick }: LeftPanelChildProps) => {
   return (
-    <LeftPanel title="Layers: Rhino Models">
+    <LeftPanel title="Layers: Rhino Models" handleClick={handleClick}>
       <div>This feature is under construction.</div>
     </LeftPanel>
   );
 };
 
-const BIMModels = () => {
+const BIMModels = ({ handleClick }: LeftPanelChildProps) => {
   return (
-    <LeftPanel title="Layers: BIM Models">
+    <LeftPanel title="Layers: BIM Models" handleClick={handleClick}>
       <div>This feature is under construction.</div>
     </LeftPanel>
   );
 };
 
-const Buildings = () => {
+const Buildings = ({ handleClick }: LeftPanelChildProps) => {
   return (
-    <LeftPanel title="Controls: Buildings">
+    <LeftPanel title="Controls: Buildings" handleClick={handleClick}>
       <div>This feature is under construction.</div>
     </LeftPanel>
   );
 };
 
-const Energy = () => {
+const Energy = ({ handleClick }: LeftPanelChildProps) => {
   return (
-    <LeftPanel title="Controls: Energy">
+    <LeftPanel title="Controls: Energy" handleClick={handleClick}>
       <div>This feature is under construction.</div>
     </LeftPanel>
   );
 };
 
-const ThermalComfort = () => {
+const ThermalComfort = ({ handleClick }: LeftPanelChildProps) => {
   return (
-    <LeftPanel title="Controls: Thermal Comfort">
+    <LeftPanel title="Controls: Thermal Comfort" handleClick={handleClick}>
       <div>This feature is under construction.</div>
     </LeftPanel>
   );
 };
 
-const Wind = () => {
+const Wind = ({ handleClick }: LeftPanelChildProps) => {
   return (
-    <LeftPanel title="Controls: Wind">
+    <LeftPanel title="Controls: Wind" handleClick={handleClick}>
       <div>This feature is under construction.</div>
     </LeftPanel>
   );
 };
 
-const Solar = () => {
+const Solar = ({ handleClick }: LeftPanelChildProps) => {
   return (
-    <LeftPanel title="Controls: Solar">
+    <LeftPanel title="Controls: Solar" handleClick={handleClick}>
       <div>This feature is under construction.</div>
     </LeftPanel>
   );
 };
 
-const Help = () => {
+const Help = ({ handleClick }: LeftPanelChildProps) => {
   return (
-    <LeftPanel title="Help">
+    <LeftPanel title="Help" handleClick={handleClick}>
       <div>This feature is under construction.</div>
     </LeftPanel>
   );
 };
 
-const Settings = () => {
+const Settings = ({ handleClick }: LeftPanelChildProps) => {
   return (
-    <LeftPanel title="Settings">
+    <LeftPanel title="Settings" handleClick={handleClick}>
       <div>This feature is under construction.</div>
     </LeftPanel>
   );
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -6,25 +6,34 @@ import Styles from '../styles/Navigation.module.css';
 const Sidebar = () => {
   const [activePage, setActivePage] = useState('');
   const handleClick = (page: string) => setActivePage(page);
+  const handleClose = () => setActivePage('');
 
   return (
     <div className={`${Styles['left-container']} ${Styles['nav-container']}`}>
       <LeftNav activePage={activePage} handleClick={handleClick}></LeftNav>
-      {activePage === 'about-nus-campus' && <LeftPanel.AboutNUSCampus></LeftPanel.AboutNUSCampus>}
-      {activePage === 'street-centerlines' && <LeftPanel.StreetCenterlines></LeftPanel.StreetCenterlines>}
-      {activePage === 'building-footprints' && <LeftPanel.BuildingFootprints></LeftPanel.BuildingFootprints>}
-      {activePage === 'green-spaces' && <LeftPanel.GreenSpaces></LeftPanel.GreenSpaces>}
-      {activePage === 'water-bodies' && <LeftPanel.WaterBodies></LeftPanel.WaterBodies>}
-      {activePage === 'osm-buildings' && <LeftPanel.OSMBuildings></LeftPanel.OSMBuildings>}
-      {activePage === 'rhino-models' && <LeftPanel.RhinoModels></LeftPanel.RhinoModels>}
-      {activePage === 'bim-models' && <LeftPanel.BIMModels></LeftPanel.BIMModels>}
-      {activePage === 'buildings' && <LeftPanel.Buildings></LeftPanel.Buildings>}
-      {activePage === 'energy' && <LeftPanel.Energy></LeftPanel.Energy>}
-      {activePage === 'thermal-comfort' && <LeftPanel.ThermalComfort></LeftPanel.ThermalComfort>}
-      {activePage === 'wind' && <LeftPanel.Wind></LeftPanel.Wind>}
-      {activePage === 'solar' && <LeftPanel.Solar></LeftPanel.Solar>}
-      {activePage === 'help' && <LeftPanel.Help></LeftPanel.Help>}
-      {activePage === 'settings' && <LeftPanel.Settings></LeftPanel.Settings>}
+      {activePage === 'about-nus-campus' && (
+        <LeftPanel.AboutNUSCampus handleClick={handleClose}></LeftPanel.AboutNUSCampus>
+      )}
+      {activePage === 'street-centerlines' && (
+        <LeftPanel.StreetCenterlines handleClick={handleClose}></LeftPanel.StreetCenterlines>
+      )}
+      {activePage === 'building-footprints' && (
+        <LeftPanel.BuildingFootprints handleClick={handleClose}></LeftPanel.BuildingFootprints>
+      )}
+      {activePage === 'green-spaces' && <LeftPanel.GreenSpaces handleClick={handleClose}></LeftPanel.GreenSpaces>}
+      {activePage === 'water-bodies' && <LeftPanel.WaterBodies handleClick={handleClose}></LeftPanel.WaterBodies>}
+      {activePage === 'osm-buildings' && <LeftPanel.OSMBuildings handleClick={handleClose}></LeftPanel.OSMBuildings>}
+      {activePage === 'rhino-models' && <LeftPanel.RhinoModels handleClick={handleClose}></LeftPanel.RhinoModels>}
+      {activePage === 'bim-models' && <LeftPanel.BIMModels handleClick={handleClose}></LeftPanel.BIMModels>}
+      {activePage === 'buildings' && <LeftPanel.Buildings handleClick={handleClose}></LeftPanel.Buildings>}
+      {activePage === 'energy' && <LeftPanel.Energy handleClick={handleClose}></LeftPanel.Energy>}
+      {activePage === 'thermal-comfort' && (
+        <LeftPanel.ThermalComfort handleClick={handleClose}></LeftPanel.ThermalComfort>
+      )}
+      {activePage === 'wind' && <LeftPanel.Wind handleClick={handleClose}></LeftPanel.Wind>}
+      {activePage === 'solar' && <LeftPanel.Solar handleClick={handleClose}></LeftPanel.Solar>}
+      {activePage === 'help' && <LeftPanel.Help handleClick={handleClose}></LeftPanel.Help>}
+      {activePage === 'settings' && <LeftPanel.Settings handleClick={handleClose}></LeftPanel.Settings>}
     </div>
   );
 };
