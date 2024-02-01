8c2bb3cf253b0241378bd8b3ccba86702079f54a
Lester Ong <lesterong776@gmail.com>
2024-02-01 10:25:36 +0800

    Add fields to schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,6 +3,15 @@ import { defineCollection, z } from 'astro:content';
 const buildingSchema = z.object({
   name: z.string().optional(),
   address: z.string().optional(),
+  postal: z.string().optional(),
+  floorToFloorHeight: z.string().optional(),
+  perimeterZoneDepth: z.string().optional(),
+  fenestrationType: z.string().optional(),
+  fenestrationShading: z.string().optional(),
+  northWindowToWallRatio: z.string().optional(),
+  southWindowToWallRatio: z.string().optional(),
+  eastWindowToWallRatio: z.string().optional(),
+  westWindowToWallRatio: z.string().optional(),
 });
 
 const buildingsCollection = defineCollection({
