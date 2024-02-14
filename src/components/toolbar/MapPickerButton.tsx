53265ee8560b9f156c52c8879e9a4815525a0649
Lester Ong <lesterong776@gmail.com>
2024-02-15 00:22:55 +0800

    Add functional map picker

--- src/components/toolbar/MapPickerButton.tsx
+++ src/components/toolbar/MapPickerButton.tsx
@@ -1,22 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import mapboxDark from '../../assets/mapbox-dark.png';
 import mapboxSatellite from '../../assets/mapbox-satellite.png';
 import Icons from '../Icons';
 
 const MapPickerButton = () => {
+  const [showPicker, setShowPicker] = useState(false);
+
   return (
-    <div className="map-picker-container">
-      <div className="map-picker">
-        <button className="map-picker-item">
-          <img src={mapboxDark.src} alt="" />
+    <div className="map-picker-container" onMouseLeave={() => setShowPicker(false)}>
+      <div className={`map-picker ${showPicker ? '' : 'hidden'}`}>
+        <button className="map-picker-item" id="mapbox-dark-btn">
+          <img src={mapboxDark.src} alt="Mapbox Dark" />
           Mapbox Dark
         </button>
-        <button className="map-picker-item">
-          <img src={mapboxSatellite.src} alt="" />
+        <button className="map-picker-item" id="mapbox-satellite-btn">
+          <img src={mapboxSatellite.src} alt="Mapbox Satellite" />
           Mapbox Satellite
         </button>
       </div>
-      <button className="toolbar-btn" id="map-picker-button" type="button">
+      <button className="toolbar-btn" id="map-picker-button" type="button" onClick={() => setShowPicker(!showPicker)}>
         <Icons.Picker></Icons.Picker>
         <span className="sr-only">Map Picker</span>
       </button>
