312447a87544b5465a8ffed6f26963cc00493255
Lester Ong <lesterong776@gmail.com>
2024-02-15 09:49:10 +0800

    Switch to using popover component

--- src/components/toolbar/MapPickerButton.tsx
+++ src/components/toolbar/MapPickerButton.tsx
@@ -1,28 +1,35 @@
-import React, { useState } from 'react';
+import React from 'react';
 import mapboxDark from '../../assets/mapbox-dark.png';
 import mapboxSatellite from '../../assets/mapbox-satellite.png';
 import Icons from '../Icons';
+import * as Popover from '@radix-ui/react-popover';
 
 const MapPickerButton = () => {
-  const [showPicker, setShowPicker] = useState(false);
-
   return (
-    <div className="map-picker-container" onMouseLeave={() => setShowPicker(false)}>
-      <div className={`map-picker ${showPicker ? '' : 'hidden'}`}>
-        <button className="map-picker-item" id="mapbox-dark-btn" type="button">
-          <img src={mapboxDark.src} alt="Mapbox Dark" />
-          Mapbox Dark
+    <Popover.Root>
+      <Popover.Trigger asChild>
+        <button className="toolbar-btn" id="map-picker-btn" type="button">
+          <Icons.Picker></Icons.Picker>
+          <span className="sr-only">Map Picker</span>
         </button>
-        <button className="map-picker-item" id="mapbox-satellite-btn" type="button">
-          <img src={mapboxSatellite.src} alt="Mapbox Satellite" />
-          Mapbox Satellite
-        </button>
-      </div>
-      <button className="toolbar-btn" id="map-picker-button" type="button" onClick={() => setShowPicker(!showPicker)}>
-        <Icons.Picker></Icons.Picker>
-        <span className="sr-only">Map Picker</span>
-      </button>
-    </div>
+      </Popover.Trigger>
+      <Popover.Portal>
+        <Popover.Content align="end">
+          <div className="map-picker-container">
+            <div className="map-picker">
+              <button className="map-picker-item" id="mapbox-dark-btn" type="button">
+                <img src={mapboxDark.src} alt="Mapbox Dark" />
+                Mapbox Dark
+              </button>
+              <button className="map-picker-item" id="mapbox-satellite-btn" type="button">
+                <img src={mapboxSatellite.src} alt="Mapbox Satellite" />
+                Mapbox Satellite
+              </button>
+            </div>
+          </div>
+        </Popover.Content>
+      </Popover.Portal>
+    </Popover.Root>
   );
 };
 
