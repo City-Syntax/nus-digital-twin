06fc0872d135202a704f1e30f358f831434d252f
Lester Ong <lesterong776@gmail.com>
2024-02-16 18:47:09 +0800

    Update buttons in building control

--- src/components/menu/Buildings.tsx
+++ src/components/menu/Buildings.tsx
@@ -4,8 +4,18 @@ import MenuContent from './MenuContent';
 const Buildings = () => {
   return (
     <MenuContent title="Controls: Buildings">
-      <div>
-        <button className="menu-btn-secondary">Select central location</button>
+      <div className="menubar-content-body">
+        <div>
+          <h3>Color By</h3>
+          <div className="btn-group">
+            <button className="active">Distance</button>
+            <button>Building Type</button>
+          </div>
+        </div>
+        <div>
+          <h3>Options</h3>
+          <button className="menu-btn-secondary">Select central location</button>
+        </div>
       </div>
     </MenuContent>
   );
