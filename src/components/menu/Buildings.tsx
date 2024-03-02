c87890c4d5e874d9be240aee04d6081c076fa678
lesterong <84223259+lesterong@users.noreply.github.com>
2024-03-02 23:41:21 +0800

    Color buildings (#9)

--- src/components/menu/Buildings.tsx
+++ src/components/menu/Buildings.tsx
@@ -1,10 +1,45 @@
 import React from 'react';
 import MenuContent from './MenuContent';
+import { buildingColorSetting, isSelectColorByDistance } from '../../store';
+import { useStore } from '@nanostores/react';
 
 const Buildings = () => {
+  const $buildingColorSetting = useStore(buildingColorSetting);
+  const $isSelectColorByDistance = useStore(isSelectColorByDistance);
   return (
     <MenuContent title="Controls: Buildings">
-      <p>This feature is under construction.</p>
+      <div className="menubar-content-body">
+        <div>
+          <h3>Color By</h3>
+          <div className="btn-group">
+            <button
+              className={`${$buildingColorSetting === 'distance' ? 'active' : ''}`}
+              onClick={() => buildingColorSetting.set('distance')}
+            >
+              Distance
+            </button>
+            <button
+              className={`${$buildingColorSetting === '' ? 'active' : ''}`}
+              onClick={() => buildingColorSetting.set('')}
+            >
+              Off
+            </button>
+          </div>
+        </div>
+        {$buildingColorSetting === 'distance' && (
+          <div>
+            <h3>Options</h3>
+            <div className="btn-group">
+              <button
+                className={`${$isSelectColorByDistance ? 'active' : ''}`}
+                onClick={() => isSelectColorByDistance.set(!$isSelectColorByDistance)}
+              >
+                Select{$isSelectColorByDistance && 'ing'} central location
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
     </MenuContent>
   );
 };
