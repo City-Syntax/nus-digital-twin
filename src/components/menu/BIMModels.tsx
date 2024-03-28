71edb4809fcf879b31d688740b637c8bbbd82eaa
lesterong <84223259+lesterong@users.noreply.github.com>
2024-03-28 13:31:27 +0800

    Add BIM toggle (#13)

--- src/components/menu/BIMModels.tsx
+++ src/components/menu/BIMModels.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import MenuContent from './MenuContent';
+import { buildingDataLayer } from '../../store';
+import { useStore } from '@nanostores/react';
 
 const BIMModels = () => {
+  const $buildingDataLayer = useStore(buildingDataLayer);
   return (
     <MenuContent title="Layers: BIM Models">
-      <p>This feature is under construction.</p>
+      <div>
+        Displays Building Information Models (BIM), a highly detailed digital building model.
+        <br />
+        Selecting BIM will disable any existing coloring effects applied.
+      </div>
+      <div>
+        <div className="btn-group">
+          <button onClick={() => buildingDataLayer.set('bim')} className={$buildingDataLayer === 'bim' ? 'active' : ''}>
+            On
+          </button>
+          <button onClick={() => buildingDataLayer.set('')} className={$buildingDataLayer !== 'bim' ? 'active' : ''}>
+            Off
+          </button>
+        </div>
+      </div>
     </MenuContent>
   );
 };
--- src/components/menu/Distance.tsx
+++ src/components/menu/Distance.tsx
@@ -1,30 +1,39 @@
 import React from 'react';
 import MenuContent from './MenuContent';
-import { buildingColorSetting, isSelectColorByDistance } from '../../store';
+import { buildingColorSetting, buildingDataLayer, isSelectColorByDistance } from '../../store';
 import { useStore } from '@nanostores/react';
 
 const Distance = () => {
   const $buildingColorSetting = useStore(buildingColorSetting);
   const $isSelectColorByDistance = useStore(isSelectColorByDistance);
+  const $buildingDataLayer = useStore(buildingDataLayer);
   return (
     <MenuContent title="Controls: Distance">
       <div>
         <h3>Color by distance</h3>
-        <div className="hint">Colors buildings based on the proximity to the selected building.</div>
-        <div className="btn-group">
-          <button
-            className={`${$buildingColorSetting === 'distance' ? 'active' : ''}`}
-            onClick={() => buildingColorSetting.set('distance')}
-          >
-            On
-          </button>
-          <button
-            className={`${$buildingColorSetting === '' ? 'active' : ''}`}
-            onClick={() => buildingColorSetting.set('')}
-          >
-            Off
-          </button>
-        </div>
+        {$buildingDataLayer !== 'osm' ? (
+          <div className="hint">Color effects are only available on OpenStreetMaps models.</div>
+        ) : (
+          <>
+            <div className="hint">Colors buildings based on the proximity to the selected building.</div>
+            <div className="btn-group">
+              <button
+                className={`${$buildingColorSetting === 'distance' ? 'active' : ''}`}
+                onClick={() => buildingColorSetting.set('distance')}
+                disabled={$buildingDataLayer !== 'osm'}
+              >
+                On
+              </button>
+              <button
+                className={`${$buildingColorSetting === '' ? 'active' : ''}`}
+                onClick={() => buildingColorSetting.set('')}
+                disabled={$buildingDataLayer !== 'osm'}
+              >
+                Off
+              </button>
+            </div>
+          </>
+        )}
       </div>
       {$buildingColorSetting === 'distance' && (
         <div>
