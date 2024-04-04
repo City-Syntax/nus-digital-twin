ffb54af7877e6802e6c8d17bf6e3f2055b28b4c1
Lester Ong <lesterong776@gmail.com>
2024-04-04 18:41:30 +0800

    Add basic carousel structure

---
+++ src/components/carousel/Carousel.tsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+import useEmblaCarousel from 'embla-carousel-react';
+
+const Carousel = () => {
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: false });
+  const [prevBtnDisabled, setPrevBtnDisabled] = useState(true);
+  const [nextBtnDisabled, setNextBtnDisabled] = useState(true);
+  const onSelect = (emblaApi: any) => {
+    setPrevBtnDisabled(!emblaApi.canScrollPrev());
+    setNextBtnDisabled(!emblaApi.canScrollNext());
+  };
+  useEffect(() => {
+    if (!emblaApi) {
+      return;
+    }
+
+    onSelect(emblaApi);
+    emblaApi.on('reInit', onSelect);
+    emblaApi.on('select', onSelect);
+  }, [emblaApi, onSelect]);
+
+  return (
+    <div className="carousel">
+      <div className="carousel-content" ref={emblaRef}>
+        <div className="carousel-content-container">
+          <div className="carousel-item">Slide 1</div>
+          <div className="carousel-item">Slide 2</div>
+          <div className="carousel-item">Slide 3</div>
+        </div>
+      </div>
+      <button onClick={() => emblaApi?.scrollPrev()} disabled={prevBtnDisabled}>
+        Previous
+      </button>
+      <button onClick={() => emblaApi?.scrollNext()} disabled={nextBtnDisabled}>
+        Next
+      </button>
+    </div>
+  );
+};
+
+export default Carousel;
--- src/components/menu/AboutNUSCampus.tsx
+++ src/components/menu/AboutNUSCampus.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import MenuContent from './MenuContent';
+import Carousel from '../carousel/Carousel';
 
 const AboutNUSCampus = () => {
   return (
     <MenuContent title="About NUS Campus">
+      <Carousel></Carousel>
       <p>Hello World!</p>
     </MenuContent>
   );
