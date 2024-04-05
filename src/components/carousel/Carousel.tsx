75d69397133c9297133db6056b43bba0cc29492c
Lester Ong <lesterong776@gmail.com>
2024-04-05 14:20:03 +0800

    Add dot buttons

--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -6,10 +6,15 @@ const Carousel = () => {
   const [emblaRef, emblaApi] = useEmblaCarousel({ loop: false });
   const [prevBtnDisabled, setPrevBtnDisabled] = useState(true);
   const [nextBtnDisabled, setNextBtnDisabled] = useState(true);
+  const [scrollSnaps, setScrollSnaps] = useState<number[]>([]);
 
   const onSelect = useCallback((emblaApi: any) => {
     setPrevBtnDisabled(!emblaApi.canScrollPrev());
     setNextBtnDisabled(!emblaApi.canScrollNext());
+  }, []);
+
+  const onInit = useCallback((emblaApi: any) => {
+    setScrollSnaps(emblaApi.scrollSnapList());
   }, []);
 
   useEffect(() => {
@@ -17,29 +22,39 @@ const Carousel = () => {
       return;
     }
 
+    onInit(emblaApi);
     onSelect(emblaApi);
+    emblaApi.on('reInit', onInit);
     emblaApi.on('reInit', onSelect);
     emblaApi.on('select', onSelect);
   }, [emblaApi, onSelect]);
 
   return (
-    <div className="carousel">
-      <div className="carousel-content" ref={emblaRef}>
-        <div className="carousel-content-container">
-          <div className="carousel-item">Slide 1</div>
-          <div className="carousel-item">Slide 2</div>
-          <div className="carousel-item">Slide 3</div>
+    <>
+      <div className="carousel">
+        <div className="carousel-content" ref={emblaRef}>
+          <div className="carousel-content-container">
+            <div className="carousel-item">Slide 1</div>
+            <div className="carousel-item">Slide 2</div>
+            <div className="carousel-item">Slide 3</div>
+            <div className="carousel-item">Slide 4</div>
+          </div>
+        </div>
+        <div className="carousel-actions">
+          <button onClick={() => emblaApi?.scrollPrev()} disabled={prevBtnDisabled}>
+            <Icons.ChevronLeft></Icons.ChevronLeft>
+          </button>
+          <button onClick={() => emblaApi?.scrollNext()} disabled={nextBtnDisabled}>
+            <Icons.ChevronRight></Icons.ChevronRight>
+          </button>
         </div>
       </div>
-      <div className="carousel-actions">
-        <button onClick={() => emblaApi?.scrollPrev()} disabled={prevBtnDisabled}>
-          <Icons.ChevronLeft></Icons.ChevronLeft>
-        </button>
-        <button onClick={() => emblaApi?.scrollNext()} disabled={nextBtnDisabled}>
-          <Icons.ChevronRight></Icons.ChevronRight>
-        </button>
+      <div className="carousel-dots">
+        {scrollSnaps.map((_, index) => (
+          <button></button>
+        ))}
       </div>
-    </div>
+    </>
   );
 };
 
