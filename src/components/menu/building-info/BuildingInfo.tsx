07f283b12069af350502b55d7dd416aac3ac7c83
Lester Ong <lesterong776@gmail.com>
2024-05-29 16:04:42 +0800

    Refactor handling of selected building

--- src/components/menu/building-info/BuildingInfo.tsx
+++ src/components/menu/building-info/BuildingInfo.tsx
@@ -1,24 +1,30 @@
 import React, { useState } from 'react';
 import MenuContent from '../MenuContent';
-import { buildingProperties } from '../../../store';
+import { buildingId, searchQuery } from '../../../store';
 import { useStore } from '@nanostores/react';
 import { SECTIONS_TO_DISPLAY, TITLE_MAPPINGS, type Categories, CATEGORY_MAPPINGS } from './buildingInfoUtils';
 import CategorySelect from './CategorySelect';
 import type { BuildingPropertiesProps } from '../../../content/config';
+import buildingsData from '../../../content/buildings/buildings.json';
 
 const BuildingInfo = () => {
   const [category, setCategory] = useState<Categories>('general');
-  const $buildingProperties = useStore(buildingProperties);
-  const selectedProperties = Object.entries($buildingProperties).filter((data) =>
+  const $buildingId = useStore(buildingId);
+  const buildingProperties = buildingsData.filter((d) => d.elementId == $buildingId)[0];
+  const propertiesToDisplay = Object.entries(buildingProperties).filter((data) =>
     SECTIONS_TO_DISPLAY[category].includes(data[0] as keyof BuildingPropertiesProps),
   );
-  buildingProperties.listen(() => setCategory('general'));
+
+  buildingId.listen(() => setCategory('general'));
+
+  // Prevent two components from being updated at the same time
+  setTimeout(() => searchQuery.set(buildingProperties.name));
 
   return (
-    <MenuContent title={$buildingProperties.name || 'Unknown Building Name'}>
+    <MenuContent title={buildingProperties.name}>
       <CategorySelect value={category} onValueChange={(value: Categories) => setCategory(value)}></CategorySelect>
-      {selectedProperties.length < 1 && <p>No information under '{CATEGORY_MAPPINGS[category]}' yet.</p>}
-      {selectedProperties.map((data) => {
+      {propertiesToDisplay.length < 1 && <p>No information under '{CATEGORY_MAPPINGS[category]}' yet.</p>}
+      {propertiesToDisplay.map((data) => {
         const title = data[0] as keyof BuildingPropertiesProps;
         return (
           <div key={data[0]}>
--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,4 @@
-import { atom, map } from 'nanostores';
-import type { BuildingPropertiesProps } from './content/config';
+import { atom } from 'nanostores';
 
 export const activeMenu = atom('');
 export const searchQuery = atom('');
@@ -9,7 +8,7 @@ export const mapLayer = atom('');
 export const buildingDataLayer = atom('osm');
 export const buildingColorSetting = atom('');
 
-export const buildingProperties = map<BuildingPropertiesProps>({});
+export const buildingId = atom('');
 export const isSelectColorByDistance = atom(false);
 export const colorByDistancePosition = atom({
   latitude: 1.29563,
