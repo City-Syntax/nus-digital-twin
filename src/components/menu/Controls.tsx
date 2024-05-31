b9c98a6f9d09648a56e0673cfc92528667abfe07
Lester Ong <lesterong776@gmail.com>
2024-05-31 20:10:48 +0800

    Fix menu bottom

--- src/components/menu/Controls.tsx
+++ src/components/menu/Controls.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
-import MenuContent from './MenuContent';
 import MenuLink from './MenuLink';
+import CloseButton from './CloseButton';
 
 const Controls = () => {
   return (
-    <MenuContent title="Controls">
-      <div className="menu-list">
-        <MenuLink label="Buildings"></MenuLink>
-        <MenuLink label="Energy"></MenuLink>
-        <MenuLink label="Thermal Comfort"></MenuLink>
-        <MenuLink label="Wind"></MenuLink>
-        <MenuLink label="Solar"></MenuLink>
-        <MenuLink label="Distance"></MenuLink>
+    <>
+      <div className="menubar-content-header">
+        <h2>Controls</h2>
+        <CloseButton></CloseButton>
       </div>
-    </MenuContent>
+      <div className="menubar-content-body">
+        <div className="menu-list">
+          <MenuLink label="Buildings"></MenuLink>
+          <MenuLink label="Energy"></MenuLink>
+          <MenuLink label="Thermal Comfort"></MenuLink>
+          <MenuLink label="Wind"></MenuLink>
+          <MenuLink label="Solar"></MenuLink>
+          <MenuLink label="Distance"></MenuLink>
+        </div>
+      </div>
+    </>
   );
 };
 
