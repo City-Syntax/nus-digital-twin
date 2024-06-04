aabf4c834ef1fa11ccfe8116d2a610179aef0f81
lesterong <84223259+lesterong@users.noreply.github.com>
2024-06-05 02:25:01 +0800

    Improve fuzzy search (#42)

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,14 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import fuzzysort from 'fuzzysort';
 import { Command } from 'cmdk';
 import Icons from './Icons';
 import buildingsData from '../content/buildings/buildings.json';
 import { activePage, buildingId, searchQuery } from '../store';
 import { useStore } from '@nanostores/react';
-import Fuse from 'fuse.js';
 
 const Searchbar = () => {
   const [open, setOpen] = useState(false);
   const $searchQuery = useStore(searchQuery);
+  const listRef = useRef<HTMLDivElement>(null);
+
+  // https://github.com/pacocoursey/cmdk/issues/234
+  const scrollUpOnChange = useCallback(() => {
+    requestAnimationFrame(() => {
+      listRef!.current!.scrollTo({ top: 0 });
+    });
+  }, []);
+
   buildingId.listen((newId) => {
     if (newId === '') {
       return;
@@ -39,16 +48,14 @@ const Searchbar = () => {
     return () => document.removeEventListener('keydown', down);
   }, []);
 
-  const fuse = new Fuse(buildingsData, {
-    keys: ['name'],
-  });
-
   let buildingsDataToShow;
+  const fuzzyResults = fuzzysort.go($searchQuery, buildingsData, { key: 'name' });
   if ($searchQuery.length === 0) {
     buildingsDataToShow = buildingsData;
   } else {
-    buildingsDataToShow = fuse.search($searchQuery).map((res) => res.item);
+    buildingsDataToShow = fuzzyResults.map((res) => res.obj);
   }
+  const searchResults = fuzzyResults.map((res) => res.highlight((m, i) => <mark key={i}>{m}</mark>));
 
   return (
     <Command shouldFilter={false} label="Search buildings" loop>
@@ -64,14 +71,17 @@ const Searchbar = () => {
             setOpen(false);
           }}
           value={$searchQuery}
-          onValueChange={(value) => searchQuery.set(value)}
+          onValueChange={(value) => {
+            searchQuery.set(value);
+            scrollUpOnChange();
+          }}
           placeholder="Search buildings"
         ></Command.Input>
         <Icons.Search></Icons.Search>
       </div>
-      <Command.List className={open ? '' : 'hide'}>
+      <Command.List className={open ? '' : 'hide'} ref={listRef}>
         <Command.Empty>No results found.</Command.Empty>
-        {buildingsDataToShow.map((building) => {
+        {buildingsDataToShow.map((building, i) => {
           return (
             <Command.Item
               key={building.elementId}
@@ -82,7 +92,7 @@ const Searchbar = () => {
                 setOpen(false);
               }}
             >
-              {building.name}
+              {searchResults[i] ? searchResults[i] : building.name}
             </Command.Item>
           );
         })}
