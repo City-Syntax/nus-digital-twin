d1671c5b68298321fcde035201642c5370ec6470
Lester Ong <lesterong776@gmail.com>
2024-06-04 17:27:24 +0800

    Add popover style

--- src/components/CommandMenu.tsx
+++ src/components/CommandMenu.tsx
@@ -1,25 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Command } from 'cmdk';
 import Icons from './Icons';
+import { searchQuery } from '../store';
+import { useStore } from '@nanostores/react';
 
 const CommandMenu = () => {
+  const $searchQuery = useStore(searchQuery);
+
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === 'k' && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        document.querySelector('input')?.focus();
+      }
+    };
+
+    document.addEventListener('keydown', down);
+    return () => document.removeEventListener('keydown', down);
+  }, []);
+
+  const values = ['COM1', 'COM2', 'COM3'];
+
   return (
     <Command label="Search buildings">
       <div className="search">
-        <Command.Input placeholder="Search buildings"></Command.Input>
+        <Command.Input
+          value={$searchQuery}
+          onValueChange={(value) => searchQuery.set(value)}
+          placeholder="Search buildings"
+        ></Command.Input>
         <Icons.Search></Icons.Search>
       </div>
-      <Command.List>
+      <Command.List className="CommandList">
         <Command.Empty>No results found.</Command.Empty>
 
-        <Command.Group heading="Letters">
-          <Command.Item>a</Command.Item>
-          <Command.Item>b</Command.Item>
-          <Command.Separator />
-          <Command.Item>c</Command.Item>
-        </Command.Group>
-
-        <Command.Item>Apple</Command.Item>
+        {values.map((val) => {
+          return (
+            <Command.Item key={val} onSelect={() => searchQuery.set(val)}>
+              {val}
+            </Command.Item>
+          );
+        })}
       </Command.List>
     </Command>
   );
