438e591c961972b1377592e1533a985513f3eead
Lester Ong <lesterong776@gmail.com>
2024-06-18 20:11:46 +0800

    Fix link styling

--- src/components/menu/MenuRight.tsx
+++ src/components/menu/MenuRight.tsx
@@ -26,7 +26,7 @@ const MenuRight = () => {
         </div>
         <div className="menu-list">
           <div className="menu-list-title">Learnings</div>
-          <a className="menu-link" type="button" href="/tutorial">
+          <a className="menu-link" type="button" href="/tutorials">
             <Icons.Learning></Icons.Learning>
             <span>Tutorial Videos</span>
           </a>
