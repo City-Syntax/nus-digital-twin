98cad1c077df234c4633d243afebccedbdbcb56d
Lester Ong <lesterong776@gmail.com>
2024-06-21 18:08:31 +0800

    Test clear all button

--- src/components/menu/GreenSpaces.tsx
+++ src/components/menu/GreenSpaces.tsx
@@ -31,6 +31,14 @@ const GreenSpaces = () => {
             </button>
           </div>
         </div>
+        <button
+          className="hint"
+          type="button"
+          style={{ background: 'none', border: 'none' }}
+          onClick={() => activeGISLayer.set(new Set())}
+        >
+          Disable all GIS layers
+        </button>
       </div>
     </>
   );
