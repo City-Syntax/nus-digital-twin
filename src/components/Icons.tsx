846dfd10a160a8db77a74ff94664eb1a8967d8a7
Lester Ong <lesterong776@gmail.com>
2024-07-02 12:31:25 +0800

    Add report issue link

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -600,6 +600,24 @@ const Spinner = ({ ...props }) => {
   );
 };
 
+const Flag = ({ ...props }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path d="M4 15s1-1 4-1 5 2 8 2 4-1 4-1V3s-1 1-4 1-5-2-8-2-4 1-4 1z" />
+      <line x1="4" y1="22" x2="4" y2="15" />
+    </svg>
+  );
+};
+
 export default {
   Close,
   Search,
@@ -631,4 +649,5 @@ export default {
   Warning,
   Logo,
   Spinner,
+  Flag,
 };
