a928525a75355b2e45fb58b8213f781ae364f536
lesterong <84223259+lesterong@users.noreply.github.com>
2024-07-04 15:40:02 +0800

    Update help page (#79)

--- src/components/menu/Help.tsx
+++ src/components/menu/Help.tsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import CloseButton from './CloseButton';
+import Icons from '../Icons';
+import type { NavType } from '../../types';
 
-const Help = () => {
+type HelpProps = {
+  navType: NavType;
+  setNavType: (navType: NavType) => void;
+};
+
+const Help = ({ navType, setNavType }: HelpProps) => {
+  const handleClick = (controlType: NavType) => () => {
+    setNavType(controlType);
+    localStorage.setItem('helpControlType', controlType);
+  };
+
+  useEffect(() => {
+    setNavType(localStorage['helpControlType'] || 'mouse');
+  }, []);
+
   return (
     <>
       <div className="menubar-content-header">
@@ -9,10 +25,96 @@ const Help = () => {
         <CloseButton></CloseButton>
       </div>
       <div className="menubar-content-body">
-        <p>This feature is under construction.</p>
+        <div className="btn-group">
+          <button type="button" onClick={handleClick('mouse')} className={navType === 'mouse' ? 'active' : ''}>
+            Mouse
+          </button>
+          <button type="button" onClick={handleClick('touch')} className={navType === 'touch' ? 'active' : ''}>
+            Touch
+          </button>
+        </div>
+        {navType === 'mouse' ? <MouseControls /> : <TouchControls />}
       </div>
     </>
   );
 };
 
 export default Help;
+
+const MouseControls = () => {
+  return (
+    <>
+      <div>
+        <h3>Pan</h3>
+        <ul className="help__content list--bullet">
+          <li>
+            <Icons.LeftClick /> Left Click + <Icons.Drag /> Drag
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h3>Zoom</h3>
+        <ul className="help__content list--bullet">
+          <li>
+            <Icons.RightClick /> Right Click + <Icons.Drag /> Drag
+          </li>
+          <li>
+            <Icons.Scroll /> Scroll
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h3>Rotate</h3>
+        <ul className="help__content list--bullet">
+          <li>
+            <Icons.MiddleClick /> Middle Click + <Icons.Drag /> Drag
+          </li>
+          <li>
+            <kbd>CTRL</kbd> +
+            <Icons.LeftClick /> Left Click +
+            <Icons.Drag /> Drag
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+const TouchControls = () => {
+  return (
+    <>
+      <div>
+        <h3>Pan</h3>
+        <ul className="help__content list--bullet">
+          <li>
+            <Icons.DragOneFinger /> One finger drag
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h3>Zoom</h3>
+        <ul className="help__content list--bullet">
+          <li>
+            <Icons.Pinch /> Pinch with two fingers
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h3>Tilt</h3>
+        <ul className="help__content list--bullet">
+          <li>
+            <Icons.TapTwoFinger /> Two finger drag in the same direction
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h3>Rotate</h3>
+        <ul className="help__content list--bullet">
+          <li>
+            <Icons.RotateTwoFinger /> Two finger drag in the opposite direction
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
