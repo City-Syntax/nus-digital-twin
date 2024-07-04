dfe26aeac1a4eeef98570ecb9c7db5e8e4135558
Lester Ong <lesterong776@gmail.com>
2024-07-04 15:36:00 +0800

    Fix unused argument

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -642,7 +642,7 @@ const RightClick = ({ ...props }) => {
 
 const MiddleClick = ({ ...props }) => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" {...props}>
       <path
         fill="currentColor"
         d="M144 16h-32a64.07 64.07 0 0 0-64 64v96a64.07 64.07 0 0 0 64 64h32a64.07 64.07 0 0 0 64-64V80a64.07 64.07 0 0 0-64-64m48 64v24h-40V88a16 16 0 0 0-16-16V32h8a48.05 48.05 0 0 1 48 48m-80-48h8v40a16 16 0 0 0-16 16v16H64V80a48.05 48.05 0 0 1 48-48m32 192h-32a48.05 48.05 0 0 1-48-48v-56h40v16a16 16 0 0 0 16 16h16a16 16 0 0 0 16-16v-16h40v56a48.05 48.05 0 0 1-48 48"
