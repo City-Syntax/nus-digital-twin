c5e3b557b77d91af3f0af4df28c0ebc784323c6c
Lester Ong <lesterong776@gmail.com>
2024-07-04 12:39:39 +0800

    Add help instructions for mouse

--- src/components/menu/Help.tsx
+++ src/components/menu/Help.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CloseButton from './CloseButton';
+import Icons from '../Icons';
 
 const Help = () => {
+  const [controlType, setControlType] = useState<'mouse' | 'touch'>('mouse');
   return (
     <>
       <div className="menubar-content-header">
@@ -9,7 +11,68 @@ const Help = () => {
         <CloseButton></CloseButton>
       </div>
       <div className="menubar-content-body">
-        <p>This feature is under construction.</p>
+        <div className="btn-group">
+          <button
+            type="button"
+            onClick={() => setControlType('mouse')}
+            className={controlType === 'mouse' ? 'active' : ''}
+          >
+            Mouse
+          </button>
+          <button
+            type="button"
+            onClick={() => setControlType('touch')}
+            className={controlType === 'touch' ? 'active' : ''}
+          >
+            Touch
+          </button>
+        </div>
+        <div>
+          <h3>Pan</h3>
+          {controlType === 'mouse' ? (
+            <ul className="help__content list--bullet">
+              <li>
+                <Icons.LeftClick /> Left Click + <Icons.Drag /> Drag
+              </li>
+              <li>
+                <Icons.Scroll /> Scroll
+              </li>
+            </ul>
+          ) : (
+            <p>Drag with one finger</p>
+          )}
+        </div>
+        <div>
+          <h3>Zoom</h3>
+          {controlType === 'mouse' ? (
+            <ul className="help__content list--bullet">
+              <li>
+                <Icons.RightClick /> Right Click + <Icons.Drag /> Drag
+              </li>
+            </ul>
+          ) : (
+            <div className="help__content">
+              <p>Drag with one finger</p>
+            </div>
+          )}
+        </div>
+        <div>
+          <h3>Rotate</h3>
+          {controlType === 'mouse' ? (
+            <ul className="help__content list--bullet">
+              <li>
+                <Icons.MiddleClick /> Middle Click + <Icons.Drag /> Drag
+              </li>
+              <li>
+                <kbd>CTRL</kbd> +
+                <Icons.LeftClick /> Left Click +
+                <Icons.Drag /> Drag
+              </li>
+            </ul>
+          ) : (
+            <p>Drag with one finger</p>
+          )}
+        </div>
       </div>
     </>
   );
