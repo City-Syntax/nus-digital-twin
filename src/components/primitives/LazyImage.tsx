d4767972611065375d57afcbe3e2eb69be924895
Lester Ong <lesterong776@gmail.com>
2024-07-09 22:46:54 +0800

    Add caption for images

--- src/components/primitives/LazyImage.tsx
+++ src/components/primitives/LazyImage.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Icons from '../Icons';
 
-const LazyImage = ({ src, alt }: { src?: string; alt?: string }) => {
+const LazyImage = ({ src, alt, author = 'Someone' }: { src?: string; alt?: string; author?: string }) => {
   const PLACEHOLDER_SRC = `data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs%3D`;
   const [hasLoaded, setHasLoaded] = useState(!src);
 
@@ -13,6 +13,7 @@ const LazyImage = ({ src, alt }: { src?: string; alt?: string }) => {
         </div>
       )}
       <img onLoad={() => setHasLoaded(true)} src={src ? src : PLACEHOLDER_SRC} alt={alt || ''} />
+      {author && <div className="img-container__author">Image by {author}</div>}
     </div>
   );
 };
