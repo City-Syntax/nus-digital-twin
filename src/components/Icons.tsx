66e0cc6efbabe11b2fc49c86e17d86e91ffa297a
lesterong <84223259+lesterong@users.noreply.github.com>
2024-07-09 22:30:27 +0800

    Add screenshot button (#84)

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -772,6 +772,24 @@ const Pinch = ({ ...props }) => {
   );
 };
 
+const Camera = ({ ...props }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z" />
+      <circle cx="12" cy="13" r="4" />
+    </svg>
+  );
+};
+
 export default {
   Close,
   Search,
@@ -812,4 +830,5 @@ export default {
   TapTwoFinger,
   RotateTwoFinger,
   Pinch,
+  Camera,
 };
