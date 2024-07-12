c9e36221e2c01d5daa07f50d957d7aba631a3c07
Lester Ong <lesterong776@gmail.com>
2024-07-12 23:23:32 +0800

    Add about NUS description

--- src/components/menu/AboutNUSCampus.tsx
+++ src/components/menu/AboutNUSCampus.tsx
@@ -9,7 +9,20 @@ const AboutNUSCampus = () => {
         <CloseButton></CloseButton>
       </div>
       <div className="menubar-content-body">
-        <p>Hello World!</p>
+        <p>
+          The National University of Singapore (NUS) is Singapore's flagship university with its main campus located at
+          Kent Ridge.
+        </p>
+        <div>
+          <h3>Size</h3>
+          <p>150 hectares</p>
+        </div>
+        <div>
+          <h3>Website</h3>
+          <a className="link" rel="noreferrer" target="_blank" href="https://nus.edu.sg">
+            nus.edu.sg
+          </a>
+        </div>
       </div>
     </>
   );
