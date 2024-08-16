e0d337d47cef7480c12f7998f885f0f56bda426a
Lester Ong <lesterong776@gmail.com>
2024-08-16 23:23:51 +0800

    Fix tooltip

--- src/components/menu/Energy.tsx
+++ src/components/menu/Energy.tsx
@@ -57,102 +57,109 @@ const Energy = () => {
         <h2>Controls: Energy</h2>
         <CloseButton page="energy"></CloseButton>
       </div>
-      <div className="menubar-content-body" style={{ height: '328px' }}>
+      <div className="menubar-content-body">
         {$buildingId === '' ? (
           <p>No building selected.</p>
         ) : !data ? (
           <p>No data available for {buildingsData.filter((d) => d.elementId == $buildingId)[0].name}.</p>
         ) : (
-          <ResponsiveBar
-            theme={{
-              legends: {
-                text: {
-                  fontFamily: 'inherit',
-                  fill: 'rgba(var(--base-content))',
+          <div style={{ height: '208px' }}>
+            <ResponsiveBar
+              theme={{
+                tooltip: {
+                  wrapper: {
+                    zIndex: 1000,
+                  },
                 },
-              },
-              axis: {
-                ticks: {
+                legends: {
                   text: {
                     fontFamily: 'inherit',
                     fill: 'rgba(var(--base-content))',
                   },
                 },
-                legend: {
-                  text: {
-                    fontFamily: 'inherit',
-                    fill: 'rgba(var(--base-content))',
-                  },
-                },
-              },
-            }}
-            enableLabel={false}
-            data={data}
-            keys={['equipment', 'lighting', 'cooling', 'heating', 'hotWater']}
-            indexBy="month"
-            margin={{ top: 20, right: 10, bottom: 70, left: 70 }}
-            padding={0.3}
-            valueScale={{ type: 'linear' }}
-            indexScale={{ type: 'band', round: true }}
-            colors={getColor}
-            axisTop={null}
-            axisRight={null}
-            axisBottom={{
-              tickSize: 5,
-              tickPadding: 5,
-              tickRotation: 0,
-              truncateTickAt: 0,
-            }}
-            axisLeft={{
-              tickSize: 5,
-              tickPadding: 5,
-              tickRotation: 0,
-              legend: 'kWh',
-              legendPosition: 'middle',
-              legendOffset: -60,
-              truncateTickAt: 0,
-            }}
-            labelSkipWidth={12}
-            labelSkipHeight={12}
-            labelTextColor={{
-              from: 'color',
-              modifiers: [['brighter', 2]],
-            }}
-            tooltip={({ label, value }) => {
-              return (
-                <div className="tooltip">
-                  {label}: {value}
-                </div>
-              );
-            }}
-            legends={[
-              {
-                dataFrom: 'keys',
-                anchor: 'bottom',
-                direction: 'row',
-                justify: false,
-                translateX: -25,
-                translateY: 68,
-                itemsSpacing: 2,
-                itemWidth: 70,
-                itemHeight: 30,
-                itemDirection: 'top-to-bottom',
-                itemOpacity: 0.85,
-                symbolSize: 10,
-                effects: [
-                  {
-                    on: 'hover',
-                    style: {
-                      itemOpacity: 1,
+                axis: {
+                  ticks: {
+                    text: {
+                      fontFamily: 'inherit',
+                      fill: 'rgba(var(--base-content))',
                     },
                   },
-                ],
-              },
-            ]}
-            role="application"
-            ariaLabel="Nivo bar chart demo"
-            barAriaLabel={(e) => e.id + ': ' + e.formattedValue + ' in country: ' + e.indexValue}
-          />
+                  legend: {
+                    text: {
+                      fontFamily: 'inherit',
+                      fill: 'rgba(var(--base-content))',
+                    },
+                  },
+                },
+              }}
+              enableLabel={false}
+              data={data}
+              keys={['equipment', 'lighting', 'cooling', 'heating', 'hotWater']}
+              indexBy="month"
+              margin={{ top: 20, right: 10, bottom: 70, left: 70 }}
+              padding={0.3}
+              valueScale={{ type: 'linear' }}
+              indexScale={{ type: 'band', round: true }}
+              colors={getColor}
+              axisTop={null}
+              axisRight={null}
+              axisBottom={{
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0,
+                truncateTickAt: 0,
+              }}
+              axisLeft={{
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0,
+                legend: 'kWh',
+                legendPosition: 'middle',
+                legendOffset: -60,
+                truncateTickAt: 0,
+              }}
+              labelSkipWidth={12}
+              labelSkipHeight={12}
+              labelTextColor={{
+                from: 'color',
+                modifiers: [['brighter', 2]],
+              }}
+              tooltip={({ label, value }) => {
+                return (
+                  <div className="tooltip">
+                    {label}: {value}
+                  </div>
+                );
+              }}
+              legends={[
+                {
+                  dataFrom: 'keys',
+                  anchor: 'bottom',
+                  direction: 'row',
+                  justify: false,
+                  translateX: -25,
+                  translateY: 68,
+                  itemsSpacing: 2,
+                  itemWidth: 70,
+                  itemHeight: 30,
+                  itemDirection: 'top-to-bottom',
+                  itemOpacity: 0.85,
+                  symbolSize: 10,
+                  effects: [
+                    {
+                      on: 'hover',
+                      style: {
+                        itemOpacity: 1,
+                      },
+                    },
+                  ],
+                },
+              ]}
+              role="application"
+              ariaLabel="Nivo bar chart demo"
+              barAriaLabel={(e) => e.id + ': ' + e.formattedValue + ' in country: ' + e.indexValue}
+            />
+          </div>
         )}
       </div>
     </>
