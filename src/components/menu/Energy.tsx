a26f8eec8a98e2d3e4785282201470d2b3be2666
Lester Ong <lesterong776@gmail.com>
2024-08-17 10:36:55 +0800

    Add scroll container

--- src/components/menu/Energy.tsx
+++ src/components/menu/Energy.tsx
@@ -7,6 +7,7 @@ import { buildingId } from '../../store';
 import Select from '../primitives/Select';
 import DownloadButton from '../primitives/DownloadButton';
 import type { EnergyGraphType } from '../../types';
+import ScrollContainer from '../primitives/ScrollContainer';
 const energyData = import.meta.glob('../../content/energy/*.json');
 
 type EnergyProps = {
@@ -53,123 +54,125 @@ const Energy = ({ graphType, setGraphType }: EnergyProps) => {
         <h2>Controls: Energy</h2>
         <CloseButton page="energy"></CloseButton>
       </div>
-      <div className="menubar-content-body">
-        {$buildingId === '' ? (
-          <p>No building selected.</p>
-        ) : (
-          <>
-            <Select<EnergyGraphType>
-              value={graphType}
-              onValueChange={(value) => {
-                setGraphType(value);
-              }}
-              options={[
-                { id: 'eu', label: 'Energy Use' },
-                { id: 'eui', label: 'Energy Use Intensity' },
-              ]}
-            ></Select>
-            {!data ? (
-              <p>
-                No {graphType === 'eu' ? 'energy use' : 'energy use intensity'} data available for{' '}
-                {buildingProperties.name}.
-              </p>
-            ) : (
-              <div style={{ height: '208px' }}>
-                <ResponsiveBar
-                  theme={{
-                    legends: {
-                      text: {
-                        fontFamily: 'inherit',
-                        fill: 'rgba(var(--base-content))',
-                      },
-                    },
-                    axis: {
-                      ticks: {
+      <ScrollContainer>
+        <div className="menubar-content-body">
+          {$buildingId === '' ? (
+            <p>No building selected.</p>
+          ) : (
+            <>
+              <Select<EnergyGraphType>
+                value={graphType}
+                onValueChange={(value) => {
+                  setGraphType(value);
+                }}
+                options={[
+                  { id: 'eu', label: 'Energy Use' },
+                  { id: 'eui', label: 'Energy Use Intensity' },
+                ]}
+              ></Select>
+              {!data ? (
+                <p>
+                  No {graphType === 'eu' ? 'energy use' : 'energy use intensity'} data available for{' '}
+                  {buildingProperties.name}.
+                </p>
+              ) : (
+                <div style={{ height: '208px' }}>
+                  <ResponsiveBar
+                    theme={{
+                      legends: {
                         text: {
                           fontFamily: 'inherit',
                           fill: 'rgba(var(--base-content))',
                         },
                       },
-                      legend: {
-                        text: {
-                          fontFamily: 'inherit',
-                          fill: 'rgba(var(--base-content))',
-                        },
-                      },
-                    },
-                  }}
-                  enableLabel={false}
-                  data={data}
-                  keys={['equipment', 'lighting', 'cooling', 'heating', 'hotWater']}
-                  indexBy="month"
-                  margin={{ top: 20, right: 22, bottom: 70, left: 72 }}
-                  padding={0.3}
-                  valueScale={{ type: 'linear' }}
-                  indexScale={{ type: 'band', round: true }}
-                  colors={getColor}
-                  axisTop={null}
-                  axisRight={null}
-                  axisBottom={{
-                    tickSize: 5,
-                    tickPadding: 5,
-                    tickRotation: 0,
-                    truncateTickAt: 0,
-                  }}
-                  axisLeft={{
-                    tickSize: 3,
-                    tickPadding: 2,
-                    tickRotation: 0,
-                    legend: graphType === 'eu' ? 'kWh' : <>kWhm&sup2;</>,
-                    legendPosition: 'middle',
-                    legendOffset: -60,
-                    truncateTickAt: 0,
-                  }}
-                  tooltip={({ id, value }) => {
-                    return (
-                      <div className="tooltip">
-                        {id}: {value}
-                      </div>
-                    );
-                  }}
-                  legends={[
-                    {
-                      dataFrom: 'keys',
-                      anchor: 'bottom',
-                      direction: 'row',
-                      justify: false,
-                      translateX: -25,
-                      translateY: 68,
-                      itemsSpacing: 2,
-                      itemWidth: 70,
-                      itemHeight: 30,
-                      itemDirection: 'top-to-bottom',
-                      itemOpacity: 0.85,
-                      symbolSize: 10,
-                      effects: [
-                        {
-                          on: 'hover',
-                          style: {
-                            itemOpacity: 1,
+                      axis: {
+                        ticks: {
+                          text: {
+                            fontFamily: 'inherit',
+                            fill: 'rgba(var(--base-content))',
                           },
                         },
-                      ],
-                    },
-                  ]}
-                  role="application"
-                  ariaLabel="Nivo bar chart demo"
-                  barAriaLabel={(e) => e.id + ': ' + e.formattedValue + ' in country: ' + e.indexValue}
-                />
-              </div>
-            )}
-            {buildingProperties && buildingProperties.idfDownload && (
-              <DownloadButton
-                type="IDF"
-                files={[{ filetype: '.idf', url: buildingProperties.idfDownload }]}
-              ></DownloadButton>
-            )}
-          </>
-        )}
-      </div>
+                        legend: {
+                          text: {
+                            fontFamily: 'inherit',
+                            fill: 'rgba(var(--base-content))',
+                          },
+                        },
+                      },
+                    }}
+                    enableLabel={false}
+                    data={data}
+                    keys={['equipment', 'lighting', 'cooling', 'heating', 'hotWater']}
+                    indexBy="month"
+                    margin={{ top: 20, right: 22, bottom: 70, left: 72 }}
+                    padding={0.3}
+                    valueScale={{ type: 'linear' }}
+                    indexScale={{ type: 'band', round: true }}
+                    colors={getColor}
+                    axisTop={null}
+                    axisRight={null}
+                    axisBottom={{
+                      tickSize: 5,
+                      tickPadding: 5,
+                      tickRotation: 0,
+                      truncateTickAt: 0,
+                    }}
+                    axisLeft={{
+                      tickSize: 3,
+                      tickPadding: 2,
+                      tickRotation: 0,
+                      legend: graphType === 'eu' ? 'kWh' : <>kWhm&sup2;</>,
+                      legendPosition: 'middle',
+                      legendOffset: -60,
+                      truncateTickAt: 0,
+                    }}
+                    tooltip={({ id, value }) => {
+                      return (
+                        <div className="tooltip">
+                          {id}: {value}
+                        </div>
+                      );
+                    }}
+                    legends={[
+                      {
+                        dataFrom: 'keys',
+                        anchor: 'bottom',
+                        direction: 'row',
+                        justify: false,
+                        translateX: -25,
+                        translateY: 68,
+                        itemsSpacing: 2,
+                        itemWidth: 70,
+                        itemHeight: 30,
+                        itemDirection: 'top-to-bottom',
+                        itemOpacity: 0.85,
+                        symbolSize: 10,
+                        effects: [
+                          {
+                            on: 'hover',
+                            style: {
+                              itemOpacity: 1,
+                            },
+                          },
+                        ],
+                      },
+                    ]}
+                    role="application"
+                    ariaLabel="Nivo bar chart demo"
+                    barAriaLabel={(e) => e.id + ': ' + e.formattedValue + ' in country: ' + e.indexValue}
+                  />
+                </div>
+              )}
+              {buildingProperties && buildingProperties.idfDownload && (
+                <DownloadButton
+                  type="IDF"
+                  files={[{ filetype: '.idf', url: buildingProperties.idfDownload }]}
+                ></DownloadButton>
+              )}
+            </>
+          )}
+        </div>
+      </ScrollContainer>
     </>
   );
 };
