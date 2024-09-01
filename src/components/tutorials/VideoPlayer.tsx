edcbab2f9100e0bd3825f4c3ffa1e6e7324e10d9
Lester Ong <lesterong776@gmail.com>
2024-09-01 11:54:21 +0800

    Update with screenshot

--- src/components/tutorials/VideoPlayer.tsx
+++ src/components/tutorials/VideoPlayer.tsx
@@ -7,19 +7,21 @@ const VideoPlayer = () => {
       <nav className="video__nav">
         <div className="video__nav-title">Playlist</div>
         <button onClick={() => setVideoLink('https://www.youtube.com/embed/dQw4w9WgXcQ')}>
-          Never gonna give you up
+          <span>1.</span>
+          <span>An introduction to Rhinoceros 3D and Basic Tools</span>
         </button>
-        <button onClick={() => setVideoLink('https://www.youtube.com/embed/y6120QOlsfU')}>Dundun Dundun Dundun</button>
+        {/* <button onClick={() => setVideoLink('https://www.youtube.com/embed/y6120QOlsfU')}>Dundun Dundun Dundun</button> */}
       </nav>
       <div className="video">
-        <iframe
+        <img src="/video-screen1.png" />
+        {/* <iframe
           src={videoLink}
           title="YouTube video player"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           referrerPolicy="strict-origin-when-cross-origin"
           allowFullScreen
-        ></iframe>
+        ></iframe> */}
       </div>
     </main>
   );
