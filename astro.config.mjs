4bbe3609ff4d51528f414a541d844bca7d2c8d38
lesterong <84223259+lesterong@users.noreply.github.com>
2024-10-09 11:53:41 +0800

    Move to hybrid rendering (#119)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   site: 'https://www.nus-digital-twin.com',
+  output: 'hybrid',
+  adapter: vercel(),
 });
