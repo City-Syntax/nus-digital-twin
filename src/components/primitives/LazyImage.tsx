20cc879e537326161844077a98e5bbbe0c889440
Lester Ong <lesterong776@gmail.com>
2024-10-17 19:25:51 +0800

    Move astro image import

--- src/components/primitives/LazyImage.tsx
+++ src/components/primitives/LazyImage.tsx
@@ -2,9 +2,10 @@ import { useEffect, useState } from 'react';
 import Icons from '../Icons';
 import type { ImageProps } from '../../types';
 
+const astroImages = import.meta.glob<{ default: ImageMetadata }>('/src/assets/**/*.{jpeg,jpg,png,gif}');
+
 const LazyImage = ({ img, alt, caption }: { img?: ImageProps; alt?: string; caption?: string }) => {
   const [hasLoaded, setHasLoaded] = useState(!img);
-  const astroImages = import.meta.glob<{ default: ImageMetadata }>('/src/assets/**/*.{jpeg,jpg,png,gif}');
   const [src, setSrc] = useState('data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs%3D');
 
   useEffect(() => {
