83e673f6621a27956e6bc6b15854cf2533030790
Lester Ong <lesterong776@gmail.com>
2024-11-28 00:15:34 +0800

    Fix scroll viewport fade

--- src/components/primitives/ScrollContainer.tsx
+++ src/components/primitives/ScrollContainer.tsx
@@ -4,45 +4,45 @@ import * as ScrollArea from '@radix-ui/react-scroll-area';
 const ScrollContainer = ({ children }: { children: React.ReactNode }) => {
   const ref = useRef<HTMLDivElement | null>(null);
 
-  useEffect(() => {
-    const scrollViewport = ref.current as HTMLDivElement;
-    if (scrollViewport.scrollHeight > scrollViewport.clientHeight) {
+  const controlOverlay = (scrollViewport: HTMLDivElement) => {
+    if (scrollViewport.scrollHeight <= scrollViewport.clientHeight) {
+      scrollViewport.classList.remove('overlay-top');
+      scrollViewport.classList.remove('overlay-bottom');
+      return;
+    }
+
+    const isTop = scrollViewport.scrollTop == 0;
+    const isBottom = Math.ceil(scrollViewport.scrollTop + scrollViewport.offsetHeight) >= scrollViewport.scrollHeight;
+
+    if (isTop) {
+      scrollViewport.classList.remove('overlay-top');
+      scrollViewport.classList.add('overlay-bottom');
+    } else if (isBottom) {
+      scrollViewport.classList.remove('overlay-bottom');
+      scrollViewport.classList.add('overlay-top');
+    } else {
+      scrollViewport.classList.add('overlay-top');
       scrollViewport.classList.add('overlay-bottom');
     }
+  };
+
+  useEffect(() => {
+    const scrollViewport = ref.current as HTMLDivElement;
+    controlOverlay(scrollViewport);
   }, [children]);
 
   useEffect(() => {
     const scrollViewport = ref.current as HTMLDivElement;
-    const controlOverlay = () => {
-      if (scrollViewport.scrollHeight <= scrollViewport.clientHeight) {
-        scrollViewport.classList.remove('overlay-top');
-        scrollViewport.classList.remove('overlay-bottom');
-        return;
-      }
-
-      const isTop = scrollViewport.scrollTop == 0;
-      const isBottom = Math.ceil(scrollViewport.scrollTop + scrollViewport.offsetHeight) >= scrollViewport.scrollHeight;
-
-      if (isTop) {
-        scrollViewport.classList.remove('overlay-top');
-        scrollViewport.classList.add('overlay-bottom');
-      } else if (isBottom) {
-        scrollViewport.classList.remove('overlay-bottom');
-        scrollViewport.classList.add('overlay-top');
-      } else {
-        scrollViewport.classList.add('overlay-top');
-        scrollViewport.classList.add('overlay-bottom');
-      }
-    };
-    const resizeObserver = new ResizeObserver(controlOverlay);
+    const resizeObserver = new ResizeObserver(() => controlOverlay(scrollViewport));
 
     resizeObserver.observe(scrollViewport);
-    scrollViewport.addEventListener('scroll', controlOverlay);
+    scrollViewport.addEventListener('scroll', () => controlOverlay(scrollViewport));
     return () => {
-      scrollViewport.removeEventListener('scroll', controlOverlay);
+      scrollViewport.removeEventListener('scroll', () => controlOverlay(scrollViewport));
       resizeObserver.disconnect();
     };
   }, []);
+
   return (
     <ScrollArea.Root className="scroll-area-root">
       <ScrollArea.Viewport ref={ref} className="scroll-area-viewport">
