9bfd7a7eaabbe182924da5a41fe4177910a35a3d
lesterong <84223259+lesterong@users.noreply.github.com>
2024-12-02 16:43:59 +0800

    Add tutorial page (#133)

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -909,6 +909,24 @@ const Api = ({ ...props }) => {
   );
 };
 
+const Video = ({ ...props }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <polygon points="23 7 16 12 23 17 23 7" />
+      <rect x="1" y="5" width="15" height="14" rx="2" ry="2" />
+    </svg>
+  );
+};
+
 const Resources = ({ ...props }) => {
   return (
     <svg
@@ -975,4 +993,5 @@ export default {
   Point,
   Api,
   Resources,
+  Video,
 };
--- src/components/menu/MenuLinks.tsx
+++ src/components/menu/MenuLinks.tsx
@@ -158,6 +158,10 @@ const Menu = ({ dir }: MenuLinksProps) => {
 const Resources = () => {
   return (
     <>
+      <a className="menu-link" href="/tutorials" target="_blank">
+        <Icons.Video></Icons.Video>
+        Tutorial Videos
+      </a>
       <a className="menu-link" href="/api/v1" target="_blank">
         <Icons.Api></Icons.Api>
         API Reference
