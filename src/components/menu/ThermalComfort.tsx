29b84c9c5d08959e3051cd283ed69fc2c6f22b05
Lester Ong <lesterong776@gmail.com>
2024-12-04 00:27:16 +0800

    Add thermal comfort charts

--- src/components/menu/ThermalComfort.tsx
+++ src/components/menu/ThermalComfort.tsx
@@ -12,9 +12,30 @@ const ThermalComfort = () => {
       <ScrollContainer>
         <div className="menubar-content-body">
           <div>
+            <h3>University Thermal Comfort Index</h3>
+            <div>
+              <LazyImage
+                ratio="13/7"
+                img={{ src: '/src/assets/thermal-comfort/University Thermal Comfort Index.png' }}
+              />
+            </div>
+          </div>
+          <div>
             <h3>Diurnal Averages</h3>
             <div>
               <LazyImage ratio="13/8" img={{ src: '/src/assets/thermal-comfort/DiurnalAverages.png' }} />
+            </div>
+          </div>
+          <div>
+            <h3>Heat Index</h3>
+            <div>
+              <LazyImage ratio="13/7" img={{ src: '/src/assets/thermal-comfort/Heat Index.png' }} />
+            </div>
+          </div>
+          <div>
+            <h3>Psychrometric Chart</h3>
+            <div>
+              <LazyImage ratio="13/8" img={{ src: '/src/assets/thermal-comfort/PsychrometricChart.png' }} />
             </div>
           </div>
         </div>
