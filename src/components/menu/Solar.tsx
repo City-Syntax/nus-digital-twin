af0bae45feaa4d3e4b2ce45ffc04bb46ae09030e
Lester Ong <lesterong776@gmail.com>
2024-12-04 00:31:18 +0800

    Add solar charts

--- src/components/menu/Solar.tsx
+++ src/components/menu/Solar.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import CloseButton from './CloseButton';
+import LazyImage from '../primitives/LazyImage';
 
 const Solar = () => {
   return (
@@ -9,7 +10,24 @@ const Solar = () => {
         <CloseButton page="solar"></CloseButton>
       </div>
       <div className="menubar-content-body">
-        <p>This feature is under construction.</p>
+        <div>
+          <h3>Global Horizontal Radiation</h3>
+          <div>
+            <LazyImage ratio="13/7" img={{ src: '/src/assets/solar/GlobalHorizontalRadiation.png' }} />
+          </div>
+        </div>
+        <div>
+          <h3>Diffuse Horizontal Radiation</h3>
+          <div>
+            <LazyImage ratio="13/7" img={{ src: '/src/assets/solar/DiffuseHorizontalRadiation.png' }} />
+          </div>
+        </div>
+        <div>
+          <h3>Direct Normal Radiation</h3>
+          <div>
+            <LazyImage ratio="13/7" img={{ src: '/src/assets/solar/DirectNormalRadiation.png' }} />
+          </div>
+        </div>
       </div>
     </>
   );
