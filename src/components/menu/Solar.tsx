f676151bd3abe333fb1368438f90117cd171b70a
Lester Ong <lesterong776@gmail.com>
2024-12-04 00:34:34 +0800

    Add ScrollContainer

--- src/components/menu/Solar.tsx
+++ src/components/menu/Solar.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
 import CloseButton from './CloseButton';
 import LazyImage from '../primitives/LazyImage';
+import ScrollContainer from '../primitives/ScrollContainer';
 
 const Solar = () => {
   return (
@@ -9,26 +9,28 @@ const Solar = () => {
         <h2>Controls: Solar</h2>
         <CloseButton page="solar"></CloseButton>
       </div>
-      <div className="menubar-content-body">
-        <div>
-          <h3>Global Horizontal Radiation</h3>
+      <ScrollContainer>
+        <div className="menubar-content-body">
           <div>
-            <LazyImage ratio="13/7" img={{ src: '/src/assets/solar/GlobalHorizontalRadiation.png' }} />
+            <h3>Global Horizontal Radiation</h3>
+            <div>
+              <LazyImage ratio="13/7" img={{ src: '/src/assets/solar/GlobalHorizontalRadiation.png' }} />
+            </div>
+          </div>
+          <div>
+            <h3>Diffuse Horizontal Radiation</h3>
+            <div>
+              <LazyImage ratio="13/7" img={{ src: '/src/assets/solar/DiffuseHorizontalRadiation.png' }} />
+            </div>
+          </div>
+          <div>
+            <h3>Direct Normal Radiation</h3>
+            <div>
+              <LazyImage ratio="13/7" img={{ src: '/src/assets/solar/DirectNormalRadiation.png' }} />
+            </div>
           </div>
         </div>
-        <div>
-          <h3>Diffuse Horizontal Radiation</h3>
-          <div>
-            <LazyImage ratio="13/7" img={{ src: '/src/assets/solar/DiffuseHorizontalRadiation.png' }} />
-          </div>
-        </div>
-        <div>
-          <h3>Direct Normal Radiation</h3>
-          <div>
-            <LazyImage ratio="13/7" img={{ src: '/src/assets/solar/DirectNormalRadiation.png' }} />
-          </div>
-        </div>
-      </div>
+      </ScrollContainer>
     </>
   );
 };
--- src/components/menu/Wind.tsx
+++ src/components/menu/Wind.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
 import CloseButton from './CloseButton';
 import LazyImage from '../primitives/LazyImage';
+import ScrollContainer from '../primitives/ScrollContainer';
 
 const Wind = () => {
   return (
@@ -9,20 +9,22 @@ const Wind = () => {
         <h2>Controls: Wind</h2>
         <CloseButton page="wind"></CloseButton>
       </div>
-      <div className="menubar-content-body">
-        <div>
-          <h3>Wind Speed</h3>
+      <ScrollContainer>
+        <div className="menubar-content-body">
           <div>
-            <LazyImage ratio="13/7" img={{ src: '/src/assets/wind/Wind Speed.png' }} />
+            <h3>Wind Speed</h3>
+            <div>
+              <LazyImage ratio="13/7" img={{ src: '/src/assets/wind/Wind Speed.png' }} />
+            </div>
+          </div>
+          <div>
+            <h3>Wind Rose</h3>
+            <div>
+              <LazyImage ratio="13/8" img={{ src: '/src/assets/wind/WindRose.png' }} />
+            </div>
           </div>
         </div>
-        <div>
-          <h3>Wind Rose</h3>
-          <div>
-            <LazyImage ratio="13/8" img={{ src: '/src/assets/wind/WindRose.png' }} />
-          </div>
-        </div>
-      </div>
+      </ScrollContainer>
     </>
   );
 };
