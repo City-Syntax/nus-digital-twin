5452a89eb7c32205f0c8955221ca6200dc573dca
Lester Ong <lesterong776@gmail.com>
2024-12-18 23:13:06 +0800

    Update vercel adapter import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import starlight from '@astrojs/starlight';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
