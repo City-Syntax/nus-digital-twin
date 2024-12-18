9c49ba5a8048382c437c8334564c7b9903754900
Lester Ong <lesterong776@gmail.com>
2024-12-18 11:19:17 +0800

    Preload fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import starlight from '@astrojs/starlight';
 import vercel from '@astrojs/vercel/serverless';
+import figtreeWoff2 from '@fontsource-variable/figtree/files/figtree-latin-wght-normal.woff2?url';
+import interWoff2 from '@fontsource-variable/inter/files/inter-latin-wght-normal.woff2?url';
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,6 +16,28 @@ export default defineConfig({
         SiteTitle: './src/components/docs/SiteTitle.astro',
       },
       customCss: ['@fontsource-variable/inter', '@fontsource-variable/figtree'],
+      head: [
+        {
+          tag: 'link',
+          attrs: {
+            rel: 'preload',
+            as: 'font',
+            type: 'font/woff2',
+            href: figtreeWoff2,
+            crossOrigin: 'anonymous',
+          },
+        },
+        {
+          tag: 'link',
+          attrs: {
+            rel: 'preload',
+            as: 'font',
+            type: 'font/woff2',
+            href: interWoff2,
+            crossOrigin: 'anonymous',
+          },
+        },
+      ],
       sidebar: [
         {
           slug: 'user-guide',
