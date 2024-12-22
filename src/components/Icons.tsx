9e51b23e794996ca9d3fd78b246706955f008ecb
Lester Ong <lesterong776@gmail.com>
2024-12-23 00:45:15 +0800

    Add developer guide to resources

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -963,6 +963,24 @@ const Guide = ({ ...props }) => {
   );
 };
 
+const Code = ({ ...props }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <polyline points="16 18 22 12 16 6" />
+      <polyline points="8 6 2 12 8 18" />
+    </svg>
+  );
+};
+
 export default {
   Close,
   Search,
@@ -1013,4 +1031,5 @@ export default {
   Resources,
   Video,
   Guide,
+  Code,
 };
