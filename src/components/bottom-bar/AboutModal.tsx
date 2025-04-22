17b5fd86bef6ca75ba4b3df151ece0b793ddb369
Lester Ong <lesterong776@gmail.com>
2025-04-22 13:12:45 +0800

    Update about modal

--- src/components/bottom-bar/AboutModal.tsx
+++ src/components/bottom-bar/AboutModal.tsx
@@ -12,11 +12,20 @@ const AboutModal = () => {
         <Dialog.Content className="modal__content">
           <Dialog.Title>About NUS Digital Twin</Dialog.Title>
           <Dialog.Description>
-            NUS Digital Twin is developed by{' '}
+            NUS Digital Twin is an{' '}
+            <a className="link" href="http://github.com/city-Syntax/nus-digital-twin" target="_blank" rel="noreferrer">
+              open-source project
+            </a>{' '}
+            developed by{' '}
             <a className="link" href="https://www.citysyntax.io" target="_blank" rel="noreferrer">
               City Syntax Lab
             </a>
-            .
+            .<h3>Conferences</h3>
+            <div>
+              Ang, Y.Q., Ong, L., Teo, J., Gan, J.L. (2024) Crowdsourced Campus Digital Twin for Building Performance
+              Simulation Teaching and Research.{' '}
+              <i>The 5th Asia Conference of International Building Performance Simulation Association 2024</i>.
+            </div>
           </Dialog.Description>
         </Dialog.Content>
       </Dialog.Portal>
