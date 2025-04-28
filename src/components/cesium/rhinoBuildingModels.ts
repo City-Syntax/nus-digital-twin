5408c6c320839cd2f6809c3ca063d6426b4da52f
Lester Ong <lesterong776@gmail.com>
2025-04-29 00:33:29 +0800

    Move to helper function

--- src/components/cesium/rhinoBuildingModels.ts
+++ src/components/cesium/rhinoBuildingModels.ts
@@ -69,139 +69,126 @@ export async function load() {
     featureIdLabel: '54583485',
   });
 
-  const rhinoNUSMuseum = await Model.fromGltfAsync({
-    modelMatrix: Transforms.headingPitchRollToFixedFrame(
-      Cartesian3.fromDegrees(103.7724974951, 1.3014554191, 38.705815314),
-      new HeadingPitchRoll(CesiumMath.toRadians(19.5), CesiumMath.toRadians(0), CesiumMath.toRadians(0)),
-    ),
-    show: false,
-    scale: 1.1,
+  const rhinoNUSMuseum = await getModelFromGltf({
+    longitude: 103.7724974951,
+    latitude: 1.3014554191,
+    height: 38.705815314,
+    heading: 19.5,
     url: '/nus-museum/nus-museum-rhino.gltf',
     featureIdLabel: '54583930',
+    scale: 1.1,
   });
 
-  const rhinoSDE2 = await Model.fromGltfAsync({
-    modelMatrix: Transforms.headingPitchRollToFixedFrame(
-      Cartesian3.fromDegrees(103.7711425478, 1.297249525, 50.3291321597),
-      new HeadingPitchRoll(CesiumMath.toRadians(199), CesiumMath.toRadians(0), CesiumMath.toRadians(0)),
-    ),
-    show: false,
+  const rhinoSDE2 = await getModelFromGltf({
+    longitude: 103.7711425478,
+    latitude: 1.297249525,
+    height: 50.3291321597,
+    heading: 199,
     url: '/sde2/sde2-rhino.gltf',
     featureIdLabel: '140078560',
   });
 
-  const rhinoSDE3 = await Model.fromGltfAsync({
-    modelMatrix: Transforms.headingPitchRollToFixedFrame(
-      Cartesian3.fromDegrees(103.7703213411, 1.2981575567, 72.1150038858),
-      new HeadingPitchRoll(CesiumMath.toRadians(154.7), CesiumMath.toRadians(0), CesiumMath.toRadians(0)),
-    ),
+  const rhinoSDE3 = await getModelFromGltf({
+    longitude: 103.7703213411,
+    latitude: 1.2981575567,
+    height: 72.1150038858,
+    heading: 154.7,
     scale: 1.2,
-    show: false,
     url: '/sde3/sde3-rhino.gltf',
     featureIdLabel: '140079084',
   });
 
-  const rhinoSDE4 = await Model.fromGltfAsync({
-    modelMatrix: Transforms.headingPitchRollToFixedFrame(
-      Cartesian3.fromDegrees(103.7703672003, 1.2968668531, 45.0226349587),
-      new HeadingPitchRoll(CesiumMath.toRadians(108.8), CesiumMath.toRadians(0), CesiumMath.toRadians(0)),
-    ),
-    show: false,
+  const rhinoSDE4 = await getModelFromGltf({
+    longitude: 103.7703672003,
+    latitude: 1.2968668531,
+    height: 45.0226349587,
+    heading: 108.8,
     url: '/sde4/sde4-rhino.gltf',
     featureIdLabel: '628774809',
   });
 
-  const rhinoTechnoEdge = await Model.fromGltfAsync({
-    modelMatrix: Transforms.headingPitchRollToFixedFrame(
-      Cartesian3.fromDegrees(103.7714223457, 1.2979071911, 58.3863356009),
-      new HeadingPitchRoll(CesiumMath.toRadians(233.1), CesiumMath.toRadians(0), CesiumMath.toRadians(0)),
-    ),
-    show: false,
+  const rhinoTechnoEdge = await getModelFromGltf({
+    longitude: 103.7714223457,
+    latitude: 1.2979071911,
+    height: 58.3863356009,
+    heading: 233.1,
     url: '/techno-edge/techno-edge-rhino.gltf',
     featureIdLabel: '730959512',
   });
 
-  const rhinoUSC = await Model.fromGltfAsync({
-    modelMatrix: Transforms.headingPitchRollToFixedFrame(
-      Cartesian3.fromDegrees(103.775544178, 1.2997341879, 27.7347173989),
-      new HeadingPitchRoll(CesiumMath.toRadians(-2.5), CesiumMath.toRadians(0), CesiumMath.toRadians(0)),
-    ),
-    show: false,
+  const rhinoUSC = await getModelFromGltf({
+    longitude: 103.775544178,
+    latitude: 1.2997341879,
+    height: 27.7347173989,
+    heading: -2.5,
     scale: 0.9,
     url: '/usc/usc-rhino.gltf',
     featureIdLabel: '730956214',
   });
 
-  const rhinoTheDeck = await Model.fromGltfAsync({
-    modelMatrix: Transforms.headingPitchRollToFixedFrame(
-      Cartesian3.fromDegrees(103.7724884969, 1.2946662859, 65.883810451),
-      new HeadingPitchRoll(CesiumMath.toRadians(-28), CesiumMath.toRadians(0), CesiumMath.toRadians(0)),
-    ),
-    show: false,
+  const rhinoTheDeck = await getModelFromGltf({
+    longitude: 103.7724884969,
+    latitude: 1.2946662859,
+    height: 65.8838104518,
+    heading: -28,
     scale: 1.4,
     url: '/the-deck/the-deck-rhino.gltf',
     featureIdLabel: '140087193',
   });
 
-  const rhinoSSLS = await Model.fromGltfAsync({
-    modelMatrix: Transforms.headingPitchRollToFixedFrame(
-      Cartesian3.fromDegrees(103.7750945264, 1.295625549, 45.8205900698),
-      new HeadingPitchRoll(CesiumMath.toRadians(55.6), CesiumMath.toRadians(0), CesiumMath.toRadians(0)),
-    ),
-    show: false,
+  const rhinoSSLS = await getModelFromGltf({
+    longitude: 103.7750945264,
+    latitude: 1.295625549,
+    height: 45.8205900698,
+    heading: 55.6,
     url: '/ssls/ssls-rhino.gltf',
     featureIdLabel: '54619730',
   });
 
-  const rhinoKuokFoundationHouse = await Model.fromGltfAsync({
-    modelMatrix: Transforms.headingPitchRollToFixedFrame(
-      Cartesian3.fromDegrees(103.7736119902, 1.3003336117, 51.8346518242),
-      new HeadingPitchRoll(CesiumMath.toRadians(-25.5), CesiumMath.toRadians(0), CesiumMath.toRadians(0)),
-    ),
-    show: false,
+  const rhinoKuokFoundationHouse = getModelFromGltf({
+    longitude: 103.7736119902,
+    latitude: 1.3003336117,
+    height: 51.8346518242,
+    heading: -25.5,
     scale: 1.52,
     url: '/raffles-hall/kuok-foundation-house-rhino.gltf',
     featureIdLabel: '241265457',
   });
 
-  const rhinoMedicineScienceLibrary = await Model.fromGltfAsync({
-    modelMatrix: Transforms.headingPitchRollToFixedFrame(
-      Cartesian3.fromDegrees(103.7817655955, 1.2969427365, 39.6354038487),
-      new HeadingPitchRoll(CesiumMath.toRadians(-69.8), CesiumMath.toRadians(0), CesiumMath.toRadians(0)),
-    ),
-    show: false,
+  const rhinoMedicineScienceLibrary = await getModelFromGltf({
+    longitude: 103.7817655955,
+    latitude: 1.2969427365,
+    height: 39.6354038487,
+    heading: -69.8,
     url: '/msl/msl-rhino.gltf',
     featureIdLabel: '141913116',
   });
 
-  const rhinoShearesMain = await Model.fromGltfAsync({
-    modelMatrix: Transforms.headingPitchRollToFixedFrame(
-      Cartesian3.fromDegrees(103.7758329693, 1.2914051269, 44.1314822062),
-      new HeadingPitchRoll(CesiumMath.toRadians(28.7), CesiumMath.toRadians(0), CesiumMath.toRadians(0)),
-    ),
+  const rhinoShearesMain = await getModelFromGltf({
+    longitude: 103.7758329693,
+    latitude: 1.2914051269,
+    height: 44.1314822062,
+    heading: 28.7,
     scale: 1.2,
-    show: false,
     url: '/sheares-hall/sheares-main-rhino.gltf',
     featureIdLabel: '639258228',
   });
 
-  const rhinoShearesA = await Model.fromGltfAsync({
-    modelMatrix: Transforms.headingPitchRollToFixedFrame(
-      Cartesian3.fromDegrees(103.775214582, 1.2914901374, 30.437453453),
-      new HeadingPitchRoll(CesiumMath.toRadians(111.3), CesiumMath.toRadians(0), CesiumMath.toRadians(90)),
-    ),
+  const rhinoShearesA = await getModelFromGltf({
+    longitude: 103.775214582,
+    latitude: 1.2914901374,
+    height: 30.437453453,
+    heading: 111.3,
     scale: 0.4,
-    show: false,
     url: '/sheares-hall/sheares-a-rhino.gltf',
     featureIdLabel: '455250294',
   });
 
-  const rhinoRvrcG = await Model.fromGltfAsync({
-    modelMatrix: Transforms.headingPitchRollToFixedFrame(
-      Cartesian3.fromDegrees(103.7767993479, 1.2974922895, 44.840297406),
-      new HeadingPitchRoll(CesiumMath.toRadians(-70.1), CesiumMath.toRadians(0), CesiumMath.toRadians(0)),
-    ),
-    show: false,
+  const rhinoRvrcG = await getModelFromGltf({
+    longitude: 103.7767993479,
+    latitude: 1.2974922895,
+    height: 44.840297406,
+    heading: -70.1,
     url: '/rvrc/rvrc-g-rhino.gltf',
     featureIdLabel: '546382548',
   });
