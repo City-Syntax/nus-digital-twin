849e86f99f5ce2c95e6bf23c6249727640a3cee6
lesterong <84223259+lesterong@users.noreply.github.com>
2025-05-09 18:47:07 +0800

    Move more buildings to ion archives (#181)

--- src/components/cesium/rhinoBuildingModels.ts
+++ src/components/cesium/rhinoBuildingModels.ts
@@ -1,15 +1,9 @@
 import { getModelFromGltf, getModelFromUrl } from './cesiumUtils';
 
 export async function load() {
-  const rhinoE6 = await getModelFromGltf({
-    longitude: 103.7730368432,
-    latitude: 1.2991728525,
-    height: 52.20497546,
-    heading: 155.6,
-    pitch: 90,
-    url: '/e6/e6-rhino.gltf',
+  const rhinoE6 = await getModelFromUrl({
+    url: '/e6/e6-rhino-ion/tileset.json',
     featureIdLabel: '139959760',
-    scale: 1.05,
   });
 
   const rhinoE2A = await getModelFromUrl({
@@ -17,51 +11,28 @@ export async function load() {
     featureIdLabel: '628777635',
   });
 
-  const rhinoCELC = await getModelFromGltf({
-    longitude: 103.7713937579,
-    latitude: 1.2969843253,
-    height: 44.9642791517,
-    heading: 18.5,
-    url: '/celc/celc-rhino.gltf',
+  const rhinoCELC = await getModelFromUrl({
+    url: '/celc/celc-rhino-ion/tileset.json',
     featureIdLabel: '140084916',
-    scale: 0.52,
   });
 
-  const rhinoPioneerHouse = await getModelFromGltf({
-    longitude: 103.7804112385,
-    latitude: 1.2902299393,
-    height: 26.1901156884,
-    heading: 99.4,
-    url: '/pioneer-house/pioneer-house-22-25-rhino.gltf',
+  const rhinoPioneerHouse2225 = await getModelFromUrl({
+    url: '/pioneer-house/pioneer-house-22-25-rhino-ion/tileset.json',
     featureIdLabel: '238932774',
   });
 
-  const rhinoCOM2 = await getModelFromGltf({
-    longitude: 103.7743124495,
-    latitude: 1.2939952856,
-    height: 37.2996820597,
-    heading: -204,
-    url: '/com2/com2-rhino.gltf',
+  const rhinoCOM2 = await getModelFromUrl({
+    url: '/com2/com2-rhino-ion/tileset.json',
     featureIdLabel: '54619699',
-    scale: 0.43,
   });
 
-  const rhinoPgprBlock1 = await getModelFromGltf({
-    longitude: 103.7829198753,
-    latitude: 1.291489802,
-    height: 51.2208270602,
-    heading: -40,
-    url: '/pgpr/pgpr-block1-rhino.gltf',
+  const rhinoPgprBlock1 = await getModelFromUrl({
+    url: '/pgpr/pgpr-block1-rhino-ion/tileset.json',
     featureIdLabel: '238932786',
-    scale: 0.48,
   });
 
-  const rhinoAS8 = await getModelFromGltf({
-    longitude: 103.7722692363,
-    latitude: 1.296027148,
-    height: 55.2598169315,
-    heading: 109.9,
-    url: '/as8/as8-rhino.gltf',
+  const rhinoAS8 = await getModelFromUrl({
+    url: '/as8/as8-rhino-ion/tileset.json',
     featureIdLabel: '54583485',
   });
 
@@ -70,40 +41,23 @@ export async function load() {
     featureIdLabel: '54583930',
   });
 
-  const rhinoSDE2 = await getModelFromGltf({
-    longitude: 103.7711425478,
-    latitude: 1.297249525,
-    height: 50.3291321597,
-    heading: 199,
-    url: '/sde2/sde2-rhino.gltf',
+  const rhinoSDE2 = await getModelFromUrl({
+    url: '/sde2/sde2-rhino-ion/tileset.json',
     featureIdLabel: '140078560',
   });
 
-  const rhinoSDE3 = await getModelFromGltf({
-    longitude: 103.7703213411,
-    latitude: 1.2981575567,
-    height: 72.1150038858,
-    heading: 154.7,
-    scale: 1.2,
-    url: '/sde3/sde3-rhino.gltf',
+  const rhinoSDE3 = await getModelFromUrl({
+    url: '/sde3/sde3-rhino-ion/tileset.json',
     featureIdLabel: '140079084',
   });
 
-  const rhinoSDE4 = await getModelFromGltf({
-    longitude: 103.7703672003,
-    latitude: 1.2968668531,
-    height: 45.0226349587,
-    heading: 108.8,
-    url: '/sde4/sde4-rhino.gltf',
+  const rhinoSDE4 = await getModelFromUrl({
+    url: '/sde4/sde4-rhino-ion/tileset.json',
     featureIdLabel: '628774809',
   });
 
-  const rhinoTechnoEdge = await getModelFromGltf({
-    longitude: 103.7714223457,
-    latitude: 1.2979071911,
-    height: 58.3863356009,
-    heading: 233.1,
-    url: '/techno-edge/techno-edge-rhino.gltf',
+  const rhinoTechnoEdge = await getModelFromUrl({
+    url: '/techno-edge/techno-edge-rhino-ion/tileset.json',
     featureIdLabel: '730959512',
   });
 
@@ -146,12 +100,8 @@ export async function load() {
     featureIdLabel: '241265457',
   });
 
-  const rhinoMedicineScienceLibrary = await getModelFromGltf({
-    longitude: 103.7817655955,
-    latitude: 1.2969427365,
-    height: 39.6354038487,
-    heading: -69.8,
-    url: '/msl/msl-rhino.gltf',
+  const rhinoMedicineScienceLibrary = await getModelFromUrl({
+    url: '/msl/msl-rhino-ion/tileset.json',
     featureIdLabel: '141913116',
   });
 
@@ -193,7 +143,7 @@ export async function load() {
     rhinoE2A,
     rhinoE6,
     rhinoCELC,
-    rhinoPioneerHouse,
+    rhinoPioneerHouse2225,
     rhinoNUSMuseum,
     rhinoCOM2,
     rhinoPgprBlock1,
