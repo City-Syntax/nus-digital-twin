45d6e8f2d7fa64e71e81d5a5e8119595a9da7210
Lester Ong <lesterong776@gmail.com>
2025-05-14 13:06:28 +0800

    Refactor notification box styles

--- src/components/notifications/NotificationsBox.tsx
+++ src/components/notifications/NotificationsBox.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import Toast from './Toast';
 
 const NotificationsBox = () => {
   return (
-    <div className="notifications-box">
+    <div className="absolute top-14 right-0 flex flex-col gap-1 px-2 items-end md:top-8">
       <a href="https://forms.gle/SbtKCbAqF1AKzKhi7" target="_blank" rel="noreferrer" id="construction">
         This application is under construction. Report any issues or feedback here.
       </a>
