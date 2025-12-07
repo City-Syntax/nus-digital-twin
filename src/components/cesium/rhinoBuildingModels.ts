571f1ff7592721cad0532b3b6b29ae65c70e9165
rubberquackingduck <222044594+rubberquackingduck@users.noreply.github.com>
2025-12-07 22:41:44 +0800

    Attempt to load models in parallel

--- src/components/cesium/rhinoBuildingModels.ts
+++ src/components/cesium/rhinoBuildingModels.ts
@@ -1,17 +1,17 @@
 import { getModelFromCesiumIon, getModelFromGltf, getModelFromUrl } from './cesiumUtils';
 
 export async function load() {
-  const rhinoE6 = await getModelFromUrl({
+  const rhinoE6 = getModelFromUrl({
     url: '/e6/e6-rhino-ion/tileset.json',
     featureIdLabel: '139959760',
   });
 
-  const rhinoE2A = await getModelFromUrl({
+  const rhinoE2A = getModelFromUrl({
     url: '/e2a/e2a-rhino-ion/tileset.json',
     featureIdLabel: '628777635',
   });
 
-  const rhinoE7 = await getModelFromGltf({
+  const rhinoE7 = getModelFromGltf({
     latitude: 1.298697,
     longitude: 103.77293,
     height: 30,
@@ -20,117 +20,117 @@ export async function load() {
     featureIdLabel: '1060426986',
   });
 
-  const rhinoCELC = await getModelFromUrl({
+  const rhinoCELC = getModelFromUrl({
     url: '/celc/celc-rhino-ion/tileset.json',
     featureIdLabel: '140084916',
   });
 
-  const rhinoPioneerHouse2225 = await getModelFromUrl({
+  const rhinoPioneerHouse2225 = getModelFromUrl({
     url: '/pioneer-house/pioneer-house-22-25-rhino-ion/tileset.json',
     featureIdLabel: '238932774',
   });
 
-  const rhinoCOM2 = await getModelFromUrl({
+  const rhinoCOM2 = getModelFromUrl({
     url: '/com2/com2-rhino-ion/tileset.json',
     featureIdLabel: '54619699',
   });
 
-  const rhinoPgprBlock1 = await getModelFromUrl({
+  const rhinoPgprBlock1 = getModelFromUrl({
     url: '/pgpr/pgpr-block1-rhino-ion/tileset.json',
     featureIdLabel: '238932786',
   });
 
-  const rhinoAS8 = await getModelFromUrl({
+  const rhinoAS8 = getModelFromUrl({
     url: '/as8/as8-rhino-ion/tileset.json',
     featureIdLabel: '54583485',
   });
 
-  const rhinoNUSMuseum = await getModelFromUrl({
+  const rhinoNUSMuseum = getModelFromUrl({
     url: '/nus-museum/nus-museum-rhino-ion/tileset.json',
     featureIdLabel: 'NUS_MUSEUM',
   });
 
-  const rhinoSDE1 = await getModelFromUrl({
+  const rhinoSDE1 = getModelFromUrl({
     url: '/sde1/sde1-rhino-ion/tileset.json',
     featureIdLabel: '140078777',
   });
 
-  const rhinoSDE2 = await getModelFromUrl({
+  const rhinoSDE2 = getModelFromUrl({
     url: '/sde2/sde2-rhino-ion/tileset.json',
     featureIdLabel: '140078560',
   });
 
-  const rhinoSDE3 = await getModelFromUrl({
+  const rhinoSDE3 = getModelFromUrl({
     url: '/sde3/sde3-rhino-ion/tileset.json',
     featureIdLabel: '140079084',
   });
 
-  const rhinoSDE4 = await getModelFromUrl({
+  const rhinoSDE4 = getModelFromUrl({
     url: '/sde4/sde4-rhino-ion/tileset.json',
     featureIdLabel: '628774809',
   });
 
-  const rhinoTechnoEdge = await getModelFromUrl({
+  const rhinoTechnoEdge = getModelFromUrl({
     url: '/techno-edge/techno-edge-rhino-ion/tileset.json',
     featureIdLabel: '730959512',
   });
 
-  const rhinoUSC = await getModelFromUrl({
+  const rhinoUSC = getModelFromUrl({
     url: '/usc/usc-rhino-ion/tileset.json',
     featureIdLabel: '730956214',
   });
 
-  const rhinoTheDeck = await getModelFromUrl({
+  const rhinoTheDeck = getModelFromUrl({
     url: '/the-deck/the-deck-rhino-ion/tileset.json',
     featureIdLabel: '140087193',
   });
 
-  const rhinoSSLS = await getModelFromUrl({
+  const rhinoSSLS = getModelFromUrl({
     url: '/ssls/ssls-rhino-ion/tileset.json',
     featureIdLabel: '54619730',
   });
 
-  const rhinoKuokFoundationHouse = await getModelFromUrl({
+  const rhinoKuokFoundationHouse = getModelFromUrl({
     url: '/raffles-hall/kuok-foundation-house-rhino-ion/tileset.json',
     featureIdLabel: '241265457',
   });
 
-  const rhinoMedicineScienceLibrary = await getModelFromUrl({
+  const rhinoMedicineScienceLibrary = getModelFromUrl({
     url: '/msl/msl-rhino-ion/tileset.json',
     featureIdLabel: '141913116',
   });
 
-  const rhinoShearesMain = await getModelFromUrl({
+  const rhinoShearesMain = getModelFromUrl({
     url: '/sheares-hall/sheares-main-rhino-ion/tileset.json',
     featureIdLabel: '639258228',
   });
 
-  const rhinoShearesA = await getModelFromUrl({
+  const rhinoShearesA = getModelFromUrl({
     url: '/sheares-hall/sheares-a-rhino-ion/tileset.json',
     featureIdLabel: '455250294',
   });
 
-  const rhinoRvrcG = await getModelFromUrl({
+  const rhinoRvrcG = getModelFromUrl({
     url: '/rvrc/rvrc-g-rhino-ion/tileset.json',
     featureIdLabel: '546382548',
   });
 
-  const rhinoLKCMuseum = await getModelFromUrl({
+  const rhinoLKCMuseum = getModelFromUrl({
     url: '/lkc-museum/lkc-museum-rhino-ion/tileset.json',
     featureIdLabel: '54619794',
   });
 
-  const rhinoYNCArtsCenter = await getModelFromUrl({
+  const rhinoYNCArtsCenter = getModelFromUrl({
     url: '/ync/ync-arts-center-rhino-ion/tileset.json',
     featureIdLabel: '732229049',
   });
 
-  const rhinoAS7 = await getModelFromUrl({
+  const rhinoAS7 = getModelFromUrl({
     url: '/as7/as7-rhino-ion/tileset.json',
     featureIdLabel: '142079835',
   });
 
-  return [
+  return Promise.all([
     rhinoE2A,
     rhinoE6,
     rhinoE7,
@@ -156,5 +156,5 @@ export async function load() {
     rhinoLKCMuseum,
     rhinoYNCArtsCenter,
     rhinoAS7,
-  ];
+  ]);
 }
